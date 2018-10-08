cd7e497ef40c220be9b020b87726fd1fcafb54e2
alexburlton <alexburlton@hotmail.com>
2018-10-08 17:53:02 +0100

    Initial stuff
    Removed pointless player asterisks, added toggle button and basic architecture.
    Still to do:
    
     - Confirm/Reset/View Stats buttons look nicer
     - Implement stats for X01: 3-dart average, 1st/2nd/3rd dart averages, 40+/60+/etc, checkout %
     - Colour code stats and players?
     - Implement some stats for Round the Clock
     - Implement some stats for Golf
     - Implement for Matches

---
+++ Darts/src/code/screen/game/GameStatisticsPanel.java
@@ -0,0 +1,70 @@
+package code.screen.game;
+
+import java.awt.BorderLayout;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JPanel;
+
+import bean.ScrollTable;
+import code.object.Dart;
+import code.utils.DatabaseUtil;
+import object.HashMapList;
+import util.Debug;
+
+/**
+ * Shows statistics for each player in a particular game.
+ * Runs ad-hoc SQL to get the stats, because the full detail isn't readily available in memory (and would be messy to maintain)
+ */
+public abstract class GameStatisticsPanel extends JPanel
+{
+	protected HashMapList<String, Dart> hmPlayerToDarts = new HashMapList<>();
+	
+	public GameStatisticsPanel() 
+	{
+		setLayout(new BorderLayout(0, 0));
+		add(table, BorderLayout.CENTER);
+	}
+	
+	protected final ScrollTable table = new ScrollTable();
+	
+	
+	public void showStats(long gameId)
+	{
+		hmPlayerToDarts = new HashMapList<>();
+		
+		StringBuilder sbSql = new StringBuilder();
+		sbSql.append(" SELECT d.Score, d.Multiplier, d.StartingScore, rnd.RoundNumber, p.Name");
+		sbSql.append(" FROM Dart d, Participant pt, Player p, Round rnd");
+		sbSql.append(" WHERE pt.GameId = " + gameId);
+		sbSql.append(" AND rnd.ParticipantId = pt.RowId");
+		sbSql.append(" AND pt.PlayerId = p.RowId");
+		sbSql.append(" AND d.RoundId = rnd.RowId");
+		sbSql.append(" ORDER BY pt.Ordinal, rnd.RoundNumber, d.Ordinal");
+		
+		try (ResultSet rs = DatabaseUtil.executeQuery(sbSql))
+		{
+			while (rs.next())
+			{
+				int score = rs.getInt("Score");
+				int multiplier = rs.getInt("Multiplier");
+				int startingScore = rs.getInt("StartingScore");
+				
+				Dart d = new Dart(score, multiplier);
+				d.setStartingScore(startingScore);
+				
+				String playerName = rs.getString("Name");
+				
+				hmPlayerToDarts.putInList(playerName, d);
+			}
+		}
+		catch (SQLException sqle)
+		{
+			Debug.logSqlException("" + sbSql, sqle);
+		}
+		
+		buildTableModel();
+	}
+	
+	protected abstract void buildTableModel();
+}
