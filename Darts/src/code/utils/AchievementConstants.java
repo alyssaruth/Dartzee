3cb1c5e540d5b5d32cae6ecdf83d47cdef02eac8
alexburlton <alexburlton@hotmail.com>
2018-10-14 16:55:24 +0100

    GUI work
    New AchievementMedal class which will ultimately contain all the logic for the visual display of each individual achievement.
    
    Currently is able to successfully animate a circular progress bar around the achievement. I think the next steps are:
    
     - Draw the arcs in 90 degree bursts to get rid of that weird "wobble" in the animation.
     - Figure out if there's something better to do than just overwriting the scene like a flipbook - this occasionally causes it to flicker (although this will be less noticeable with a transparent background).
     - Plug this in properly for the currently existing achievement (best finish). This way I can get the design ironed out for what variables are getting passed around etc.
     - Start work on creating the interactive center part of the achievement.

--- Darts/src/code/utils/AchievementConstants.java
+++ Darts/src/code/utils/AchievementConstants.java
@@ -6,4 +6,13 @@ package code.utils;
 public interface AchievementConstants
 {
 	public static final int ACHIEVEMENT_REF_BEST_FINISH = 0;
+	
+	//Grades low-to-high
+	public static final int GRADE_NONE = 0;
+	public static final int GRADE_RED = 1;
+	public static final int GRADE_ORANGE = 2;
+	public static final int GRADE_YELLOW = 3;
+	public static final int GRADE_GREEN = 4;
+	public static final int GRADE_AQUA = 5;
+	public static final int GRADE_PINK = 6;
 }
