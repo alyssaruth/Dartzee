e3e8915399992996d969b9e7d78a4ff157528068
alexburlton <alexburlton@hotmail.com>
2019-02-10 21:14:10 +0000

    Dartzee rule unit tests
    Added unit tests for most rules so far. Renamed a couple for consistency and fixed a few issues found in tests

--- Darts/src/burlton/dartzee/code/dartzee/DartzeeDartRuleEven.kt
+++ Darts/src/burlton/dartzee/code/dartzee/DartzeeDartRuleEven.kt
@@ -1,16 +1,13 @@
 package burlton.dartzee.code.dartzee
 
 import burlton.dartzee.code.`object`.DartboardSegmentKt
-import org.w3c.dom.Element
 
 class DartzeeDartRuleEven : AbstractDartzeeDartRule()
 {
     override fun isValidSegment(segment: DartboardSegmentKt): Boolean
     {
-        return (segment.getTotal() % 2 == 0)
+        return segment.score % 2 == 0 && !segment.isMiss()
     }
 
     override fun getRuleIdentifier() = "Even"
-    override fun writeXmlAttributes(rootElement: Element) {}
-    override fun populate(rootElement: Element) {}
 }
