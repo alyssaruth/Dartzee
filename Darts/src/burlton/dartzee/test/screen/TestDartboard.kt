124e31834abfd246a99fd80ea344b3d8f8812858
alexburlton <alexburlton@hotmail.com>
2019-02-20 13:42:55 +0000

    More tests for Dartboard

--- Darts/src/burlton/dartzee/test/screen/TestDartboard.kt
+++ Darts/src/burlton/dartzee/test/screen/TestDartboard.kt
@@ -2,12 +2,16 @@ package burlton.dartzee.test.screen
 
 import burlton.core.code.util.Debug
 import burlton.core.test.TestDebug
+import burlton.dartzee.code.`object`.DEFAULT_COLOUR_WRAPPER
 import burlton.dartzee.code.`object`.Dart
 import burlton.dartzee.code.listener.DartboardListener
 import burlton.dartzee.code.screen.Dartboard
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.*
+import java.awt.Color
 import java.awt.Point
 import java.awt.event.MouseEvent
 import kotlin.test.assertNotNull
@@ -48,12 +52,12 @@ class TestDartboard
         dartboard.paintDartboardCached()
 
         assertNotNull(Dartboard.dartboardTemplate)
-        verify(dartboard, never()).initialiseFromTemplate(anyObject())
+        verify(dartboard, never()).initialiseFromTemplate()
 
         val dartboard2 = spy(Dartboard(50, 50))
         dartboard2.paintDartboardCached()
 
-        verify(dartboard2).initialiseFromTemplate(anyObject())
+        verify(dartboard2, times(1)).initialiseFromTemplate()
     }
 
     @Test
@@ -65,6 +69,32 @@ class TestDartboard
         Dartboard.appearancePreferenceChanged()
 
         assertNull(Dartboard.dartboardTemplate)
+    }
+
+
+    @Test
+    fun `Dartboard should paint the correct colours`()
+    {
+        Dartboard.dartboardTemplate = null
+
+        val dartboard = Dartboard(50, 50)
+        dartboard.paintDartboard(cached = true, colourWrapper = DEFAULT_COLOUR_WRAPPER)
+
+        val img = Dartboard.dartboardTemplate!!.getDartboardImg()
+
+        val singleTwenty = img.getRGB(25, 10)
+        val singleSix = img.getRGB(40, 25)
+        val trebleNineteen = img.getRGB(22, 42)
+        val doubleTwenty = img.getRGB(25, 8)
+        val miss = img.getRGB(25, 7)
+        val missBoard = img.getRGB(0, 0)
+
+        assertThat(Color(singleTwenty), equalTo(Color.BLACK))
+        assertThat(Color(singleSix), equalTo(Color.WHITE))
+        assertThat(Color(trebleNineteen), equalTo(Color.GREEN))
+        assertThat(Color(doubleTwenty), equalTo(Color.RED))
+        assertThat(Color(miss), equalTo(Color.BLACK))
+        assertThat(Color(missBoard), equalTo(Color.BLACK))
     }
 }
 
