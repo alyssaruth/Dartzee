627bb7fd413189b2e45a9504acc7a6fd27b7b34a
alexburlton <alexburlton@hotmail.com>
2019-03-10 16:10:00 +0000

    Converted DartsScorerX01 to kotlin Finished implementing checkout suggestions

---
+++ Darts/src/burlton/dartzee/code/screen/game/DartsScorerX01.kt
@@ -0,0 +1,252 @@
+package burlton.dartzee.code.screen.game
+
+import burlton.dartzee.code.`object`.Dart
+import burlton.dartzee.code.`object`.DartHint
+import burlton.dartzee.code.utils.DartsColour
+import burlton.dartzee.code.utils.sumScore
+import burlton.desktopcore.code.bean.AbstractTableRenderer
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Component
+import java.awt.Font
+import javax.swing.JLabel
+import javax.swing.JTable
+import javax.swing.SwingConstants
+import javax.swing.table.DefaultTableCellRenderer
+import javax.swing.table.TableModel
+
+class DartsScorerX01 : DartsScorerPausable()
+{
+    private val lblStartingScore = JLabel("X01")
+
+    fun getLatestScoreRemaining(): Int
+    {
+        val model = tableScores.model
+
+        val rowCount = model.rowCount
+        return if (rowCount == 0)
+        {
+            Integer.parseInt(lblStartingScore.text)
+        }
+        else model.getValueAt(rowCount - 1, SCORE_COLUMN) as Int
+    }
+
+    init
+    {
+        lblStartingScore.horizontalAlignment = SwingConstants.CENTER
+        lblStartingScore.font = Font("Trebuchet MS", Font.PLAIN, 16)
+        panelNorth.add(lblStartingScore, BorderLayout.CENTER)
+    }
+
+
+    override fun initImpl(gameParams: String)
+    {
+        val startingScore = Integer.parseInt(gameParams)
+        lblStartingScore.text = "$startingScore"
+
+        tableScores.getColumn(SCORE_COLUMN).cellRenderer = ScorerRenderer()
+        for (i in 0 until SCORE_COLUMN)
+        {
+            tableScores.getColumn(i).cellRenderer = DartRenderer()
+        }
+    }
+
+    override fun playerIsFinished() = getLatestScoreRemaining() == 0
+
+    /**
+     * How many darts have been thrown?
+     *
+     * 3 * (rows - 1) + #(darts in the last row)
+     */
+    override fun getTotalScore(): Int
+    {
+        val rowCount = model.rowCount
+        if (rowCount == 0)
+        {
+            return 0
+        }
+
+        var dartCount = Math.max((model.rowCount - 1) * 3, 0)
+
+        //We now use this mid-game
+        if (rowIsComplete(rowCount - 1) && !playerIsFinished())
+        {
+            return dartCount + 3
+        }
+
+        dartCount += getDartsForRow(rowCount - 1).size
+        return dartCount
+    }
+
+    private fun getDartsForRow(row: Int): List<Dart>
+    {
+        val ret = mutableListOf<Dart>()
+        for (i in 0 until SCORE_COLUMN)
+        {
+            val drt = model.getValueAt(row, i) as Dart?
+            if (drt != null)
+            {
+                ret.add(drt)
+            }
+        }
+
+        return ret
+    }
+
+    override fun rowIsComplete(rowNumber: Int) = model.getValueAt(rowNumber, SCORE_COLUMN) != null
+
+    override fun clearCurrentRound()
+    {
+        val rowCount = model.rowCount
+        if (rowCount == 0)
+        {
+            return
+        }
+
+        //If we've come into here by clicking 'pause', the latest round might be a completed one.
+        //Only clear the round if it's 'unconfirmed'.
+        val value = model.getValueAt(rowCount - 1, SCORE_COLUMN)
+        if (value == null)
+        {
+            model.removeRow(rowCount - 1)
+        }
+    }
+
+    override fun getNumberOfColumns() = SCORE_COLUMN + 1
+
+    fun finaliseRoundScore(startingScore: Int, bust: Boolean)
+    {
+        val row = model.rowCount - 1
+
+        if (bust)
+        {
+            model.setValueAt(startingScore, row, SCORE_COLUMN)
+        }
+        else
+        {
+            val dartScore = sumScore(getDartsForRow(row))
+            model.setValueAt(startingScore - dartScore, row, SCORE_COLUMN)
+        }
+    }
+
+    override fun addDart(drt: Dart)
+    {
+        removeHints()
+
+        super.addDart(drt)
+    }
+
+    fun addHint(drt: Dart)
+    {
+        super.addDart(drt)
+    }
+
+    private fun removeHints()
+    {
+        val row = model.rowCount - 1
+        if (row < 0)
+        {
+            return
+        }
+
+        for (i in 0 until SCORE_COLUMN)
+        {
+            if (model.getValueAt(row, i) is DartHint)
+            {
+                model.setValueAt(null, row, i)
+            }
+        }
+    }
+
+
+    /**
+     * Inner classes
+     */
+    private class DartRenderer : AbstractTableRenderer<Dart>()
+    {
+        override fun getReplacementValue(drt: Dart?): Any
+        {
+            return when(drt)
+            {
+                null -> ""
+                is DartHint -> "($drt)"
+                else -> "$drt"
+            }
+        }
+
+        override fun setCellColours(typedValue: Dart?, isSelected: Boolean)
+        {
+            foreground = if (isSelected)
+            {
+                if (typedValue is DartHint) Color.CYAN else Color.WHITE
+            }
+            else
+            {
+                if (typedValue is DartHint) Color.RED else Color.BLACK
+            }
+
+            val style = if (typedValue is DartHint) Font.ITALIC else Font.PLAIN
+
+            font = Font(font.name, style, font.size)
+        }
+
+        override fun allowNulls(): Boolean
+        {
+            return true
+        }
+    }
+
+    private class ScorerRenderer : DefaultTableCellRenderer()
+    {
+        override fun getTableCellRendererComponent(table: JTable?, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component
+        {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+            horizontalAlignment = SwingConstants.CENTER
+            font = Font("Trebuchet MS", Font.BOLD, 15)
+            val modelRow = table!!.convertRowIndexToModel(row)
+
+            setColours(table, modelRow)
+            return this
+        }
+
+        private fun setColours(table: JTable, modelRow: Int)
+        {
+            val tm = table.model
+            val totalScore = (getScoreAt(tm, modelRow, 0)
+                    + getScoreAt(tm, modelRow, 1)
+                    + getScoreAt(tm, modelRow, 2))
+
+            val fg = DartsColour.getScorerForegroundColour(totalScore.toDouble())
+            val bg = DartsColour.getScorerBackgroundColour(totalScore.toDouble())
+
+            foreground = fg
+            background = bg
+        }
+
+        private fun getScoreAt(tm: TableModel, row: Int, col: Int): Int
+        {
+            val value = tm.getValueAt(row, col) as Dart? ?: return 0
+            if (value is DartHint)
+            {
+                return 0
+            }
+
+            return value.getTotal()
+        }
+    }
+
+    companion object
+    {
+        private const val SCORE_COLUMN = 3
+
+        /**
+         * Static methods
+         */
+        fun factory(parent: GamePanelX01): DartsScorerX01
+        {
+            val scorer = DartsScorerX01()
+            scorer.setParent(parent)
+            return scorer
+        }
+    }
+}
--- Darts/src/burlton/dartzee/test/object/TestCheckoutSuggester.kt
+++ Darts/src/burlton/dartzee/test/object/TestCheckoutSuggester.kt
@@ -1,11 +1,15 @@
 package burlton.dartzee.test.`object`
 
 import burlton.dartzee.code.`object`.CheckoutSuggester
-import burlton.dartzee.code.`object`.Dart
+import burlton.dartzee.code.`object`.DartHint
+import burlton.dartzee.code.utils.getCheckoutScores
 import burlton.dartzee.code.utils.sumScore
 import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.matchers.collections.shouldHaveAtMostSize
+import io.kotlintest.matchers.collections.shouldHaveSingleElement
 import io.kotlintest.matchers.maps.shouldContain
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.shouldThrow
 import org.junit.Test
 
@@ -14,13 +18,13 @@ class TestCheckoutSuggester
     @Test
     fun `should parse checkouts for one, two or three darts`()
     {
-        val list = listOf("170=T20,T20,D25", "60=20,D20", "20=D10")
+        val list = listOf("170=T20,T20,D25")
 
         val map = CheckoutSuggester.parseCheckouts(list)
 
-        map.shouldContain(170, listOf(Dart(20, 3), Dart(20, 3), Dart(25, 2)))
-        map.shouldContain(60, listOf(Dart(20, 1), Dart(20, 2)))
-        map.shouldContain(20, listOf(Dart(10, 2)))
+        map.shouldContain("170-3", listOf(DartHint(20, 3), DartHint(20, 3), DartHint(25, 2)))
+        map.shouldContain("110-2", listOf(DartHint(20, 3), DartHint(25, 2)))
+        map.shouldContain("50-1", listOf(DartHint(25, 2)))
     }
 
     @Test
@@ -36,18 +40,46 @@ class TestCheckoutSuggester
     @Test
     fun `should return null if no checkout exists`()
     {
-        CheckoutSuggester.suggestCheckout(180) shouldBe null
+        CheckoutSuggester.suggestCheckout(180, 3) shouldBe null
+        CheckoutSuggester.suggestCheckout(60, 1) shouldBe null
     }
 
     @Test
     fun `should only suggest legitimate checkouts`()
     {
-        for (i in 59..171)
+        for (i in 0..171)
         {
-            val checkout = CheckoutSuggester.suggestCheckout(i) ?: continue
+            for (darts in 1..3)
+            {
+                val checkout = CheckoutSuggester.suggestCheckout(i, darts) ?: continue
 
-            sumScore(checkout) shouldBe i
-            checkout.last().isDouble().shouldBeTrue()
+                checkout.shouldHaveAtMostSize(darts)
+                sumScore(checkout) shouldBe i
+                checkout.last().isDouble().shouldBeTrue()
+            }
+        }
+    }
+
+    @Test
+    fun `should have suggestions for everything up to 150`()
+    {
+        for (i in 2..150)
+        {
+            val checkout = CheckoutSuggester.suggestCheckout(i, 3)
+            if (checkout == null)
+            {
+                println(i)
+            }
+            checkout shouldNotBe null
+        }
+    }
+
+    @Test
+    fun `should suggest straight doubles when you're on one`()
+    {
+        getCheckoutScores().forEach{
+            val checkout = CheckoutSuggester.suggestCheckout(it, 3)!!
+            checkout.shouldHaveSingleElement(DartHint(it/2, 2))
         }
     }
 }
