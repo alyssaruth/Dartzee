684b046dff11f1a13e43f37fe6d832fc225dee68
alexburlton <alexburlton@hotmail.com>
2019-03-10 17:29:34 +0000

    DartHint tests

---
+++ Darts/src/burlton/dartzee/test/object/TestDartHint.kt
@@ -0,0 +1,42 @@
+package burlton.dartzee.test.`object`
+
+import burlton.dartzee.code.`object`.DartHint
+import burlton.dartzee.code.`object`.factoryDartHintFromString
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
+import org.junit.Test
+
+class TestDartHint
+{
+    @Test
+    fun `should factory doubles`()
+    {
+        val dart = factoryDartHintFromString("D20")
+
+        dart shouldBe DartHint(20, 2)
+    }
+
+    @Test
+    fun `should factory trebles`()
+    {
+        val dart = factoryDartHintFromString("T15")
+
+        dart shouldBe DartHint(15, 3)
+    }
+
+    @Test
+    fun `should factory singles`()
+    {
+        val dart = factoryDartHintFromString("18")
+
+        dart shouldBe DartHint(18, 1)
+    }
+
+    @Test
+    fun `should throw an error for invalid input`()
+    {
+        shouldThrow<NumberFormatException>{
+            factoryDartHintFromString("X20")
+        }
+    }
+}
