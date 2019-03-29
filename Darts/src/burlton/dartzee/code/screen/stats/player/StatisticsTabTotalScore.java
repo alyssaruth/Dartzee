be5a750f59ae1e28c5ec9da1498adef7fc6bec11
alexburlton <alexburlton@hotmail.com>
2019-03-29 21:50:11 +0000

    Fix a bug where comparison graphs were in a random order for total score in player stats

--- Darts/src/burlton/dartzee/code/screen/stats/player/StatisticsTabTotalScore.java
+++ Darts/src/burlton/dartzee/code/screen/stats/player/StatisticsTabTotalScore.java
@@ -182,7 +182,10 @@ public class StatisticsTabTotalScore extends AbstractStatisticsTab
 		//Sort out what the min and max displayed on the graph will be
 		adjustGraphMinAndMax(gamesToGraph, otherGamesToGraph);
 		
-		//Populate the raw data table
+		//Sort the games and populate the raw data table
+
+		gamesToGraph.sort(GameWrapper::compareStartDate);
+		otherGamesToGraph.sort(GameWrapper::compareStartDate);
 		populateTable(gamesToGraph);
 		
 		dataset = new DefaultCategoryDataset();
@@ -290,8 +293,6 @@ public class StatisticsTabTotalScore extends AbstractStatisticsTab
 		model.addColumn("Score");
 		model.addColumn("Game");
 		model.addColumn("!Unfinished");
-		
-		gamesToGraph.sort((GameWrapper g1, GameWrapper g2) -> g1.compareStartDate(g2));
 		
 		for (int i=0; i<gamesToGraph.size(); i++)
 		{
