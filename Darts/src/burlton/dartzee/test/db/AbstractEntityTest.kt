61e38830727f0cc91c562990e036c8a2dd0edf6a
alexburlton <alexburlton@hotmail.com>
2019-04-03 08:22:49 +0100

    Refactor field logic into AbstractEntity getField/setField/getFieldType Convert TableModelEntity to kotlin and add tests Split out AbstractEntityTest into 3 so they're easier to read

--- Darts/src/burlton/dartzee/test/db/AbstractEntityTest.kt
+++ Darts/src/burlton/dartzee/test/db/AbstractEntityTest.kt
@@ -1,6 +1,5 @@
 package burlton.dartzee.test.db
 
-import burlton.core.code.util.Debug
 import burlton.core.test.helper.getLogs
 import burlton.dartzee.code.db.AbstractEntity
 import burlton.dartzee.test.helper.AbstractDartsTest
@@ -11,7 +10,6 @@ import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 import org.junit.Test
-import java.lang.reflect.Method
 import java.sql.Timestamp
 
 abstract class AbstractEntityTest<E: AbstractEntity<E>>: AbstractDartsTest()
@@ -29,7 +27,47 @@ abstract class AbstractEntityTest<E: AbstractEntity<E>>: AbstractDartsTest()
     }
 
     @Test
-    fun `Insert, Retrieve, Update, Delete`()
+    fun `Delete individual row`()
+    {
+        wipeTable(dao.getTableName())
+
+        val entity: AbstractEntity<E> = dao.javaClass.newInstance()
+        entity.assignRowId()
+        setValuesAndSaveToDatabase(entity, true)
+        getCountFromTable(dao.getTableName()) shouldBe 1
+
+        entity.deleteFromDatabase() shouldBe true
+        getCountFromTable(dao.getTableName()) shouldBe 0
+    }
+
+    @Test
+    fun `Insert and retrieve`()
+    {
+        wipeTable(dao.getTableName())
+
+        val entity: AbstractEntity<E> = dao.javaClass.newInstance()
+        entity.assignRowId()
+        val rowId = entity.rowId
+
+        setValuesAndSaveToDatabase(entity, true)
+        getLogs().shouldContain("INSERT INTO ${dao.getTableName()} VALUES ('$rowId'")
+
+        //Retrieve and check all values are as expected
+        val retrievedEntity = dao.retrieveForId(rowId)!!
+
+        getExpectedClassFields().forEach{
+            val fieldType = retrievedEntity.getFieldType(it)
+            val retrievedValue = retrievedEntity.getField(it)
+
+            retrievedValue shouldBe getValueForField(fieldType, true)
+        }
+
+        entity.dtCreation shouldBe retrievedEntity.dtCreation
+        retrievedEntity.dtLastUpdate.after(entity.dtCreation) shouldBe true
+    }
+
+    @Test
+    fun `Update and retrieve`()
     {
         wipeTable(dao.getTableName())
 
@@ -39,44 +77,25 @@ abstract class AbstractEntityTest<E: AbstractEntity<E>>: AbstractDartsTest()
 
         //Insert
         setValuesAndSaveToDatabase(entity, true)
-        getLogs().shouldContain("INSERT INTO ${dao.getTableName()} VALUES ('$rowId'")
-        Debug.clearLogs()
-
-        //Retrieve and check all values are as expected
-        val retrievedEntity = dao.retrieveForId(rowId)!!
-
-        getExpectedClassFields().forEach{
-            val getMethod = dao.javaClass.getMethod("get$it")
-
-            val retrievedValue = getMethod.invoke(retrievedEntity)
-            retrievedValue shouldBe getValueForField(getMethod, true)
-        }
-
-        val dtCreation = entity.dtCreation
-        val dtFirstUpdate = retrievedEntity.dtLastUpdate
-
-        retrievedEntity.dtCreation shouldBe dtCreation
-        dtFirstUpdate.after(dtCreation) shouldBe true
+        val dtFirstUpdate = entity.dtLastUpdate
+        getCountFromTable(dao.getTableName()) shouldBe 1
 
         //Update
-        setValuesAndSaveToDatabase(retrievedEntity, false)
+        setValuesAndSaveToDatabase(entity, false)
         getLogs().shouldContain("UPDATE ${dao.getTableName()}")
+        getCountFromTable(dao.getTableName()) shouldBe 1
 
-        //Final retrieve to make sure updated values are set correctly
+        //Retrieve to make sure updated values are set correctly
         val finalEntity = dao.retrieveEntity("RowId = '$rowId'")!!
         getExpectedClassFields().forEach{
-            val getMethod = dao.javaClass.getMethod("get$it")
+            val fieldType = finalEntity.getFieldType(it)
+            val retrievedValue = finalEntity.getField(it)
 
-            val retrievedValue = getMethod.invoke(finalEntity)
-            retrievedValue shouldBe getValueForField(getMethod, false)
+            retrievedValue shouldBe getValueForField(fieldType, false)
         }
 
         finalEntity.dtCreation shouldBe entity.dtCreation
         finalEntity.dtLastUpdate.after(dtFirstUpdate) shouldBe true
-
-        //Delete
-        finalEntity.deleteFromDatabase() shouldBe true
-        getCountFromTable(dao.getTableName()) shouldBe 0
     }
     private fun setValuesAndSaveToDatabase(entity: AbstractEntity<E>, initial: Boolean)
     {
@@ -84,10 +103,8 @@ abstract class AbstractEntityTest<E: AbstractEntity<E>>: AbstractDartsTest()
         Thread.sleep(50)
 
         getExpectedClassFields().forEach{
-            val getMethod = dao.javaClass.getMethod("get$it")
-            val setMethod = dao.javaClass.getDeclaredMethod("set$it", getMethod.returnType)
-
-            setMethod.invoke(entity, getValueForField(getMethod, initial))
+            val fieldType = entity.getFieldType(it)
+            entity.setField(it, getValueForField(fieldType, initial))
         }
 
         entity.saveToDatabase()
@@ -103,16 +120,16 @@ abstract class AbstractEntityTest<E: AbstractEntity<E>>: AbstractDartsTest()
         return cols
     }
 
-    private fun getValueForField(getMethod: Method, initial: Boolean): Any
+    private fun getValueForField(fieldType: Class<*>, initial: Boolean): Any
     {
-        return when (getMethod.returnType)
+        return when (fieldType)
         {
             String::class.java -> if (initial) "foo" else "bar"
             Int::class.java -> if (initial) 20 else 100
             Long::class.java -> if (initial) 2000 else Integer.MAX_VALUE - 1
             Timestamp::class.java -> if (initial) Timestamp.valueOf("2019-04-01 21:29:32") else DateStatics.END_OF_TIME
             else -> {
-                println(getMethod.returnType)
+                println(fieldType)
                 "uh oh"
             }
         }
