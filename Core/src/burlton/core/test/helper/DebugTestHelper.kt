590e99431c9320c8fe8aa9070e7b122e49a8e638
alexburlton <alexburlton@hotmail.com>
2019-04-12 20:42:00 +0100

    BulkInserter tests

--- Core/src/burlton/core/test/helper/DebugTestHelper.kt
+++ Core/src/burlton/core/test/helper/DebugTestHelper.kt
@@ -15,3 +15,11 @@ fun getLogs(): String
     Debug.waitUntilLoggingFinished()
     return Debug.getCurrentLogs()
 }
+
+fun getLogLines(): List<String>
+{
+    val logs = getLogs()
+    val lines = logs.lines().toMutableList()
+    lines.removeAt(0)
+    return lines
+}
