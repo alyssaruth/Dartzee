69612bd47e0e0a1e5771ef28e1b8b3731338a06d
alexburlton <alexburlton@hotmail.com>
2019-04-14 19:41:40 +0100

    Start on reporting tests

---
+++ Darts/src/burlton/dartzee/test/reporting/TestIncludedPlayerParameters.kt
@@ -0,0 +1,50 @@
+package burlton.dartzee.test.reporting
+
+import burlton.dartzee.code.reporting.IncludedPlayerParameters
+import burlton.dartzee.code.reporting.ReportParameters
+import burlton.dartzee.code.reporting.runReport
+import burlton.dartzee.test.helper.AbstractDartsTest
+import burlton.dartzee.test.helper.insertGame
+import burlton.dartzee.test.helper.insertParticipant
+import burlton.dartzee.test.helper.insertPlayer
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.string.shouldBeEmpty
+import org.junit.Test
+
+class TestIncludedPlayerParameters: AbstractDartsTest()
+{
+    @Test
+    fun `Should be blank by default`()
+    {
+        val ipp = IncludedPlayerParameters()
+        ipp.generateExtraWhereSql("foo").shouldBeEmpty()
+    }
+
+    @Test
+    fun `Should filter by finishing position`()
+    {
+        val player = insertPlayer("Bob")
+
+        val winningGameId = insertGame(localId = 1)
+        val thirdGameId = insertGame(localId = 2)
+        val secondGameId = insertGame(localId = 3)
+        val unfinishedGameId = insertGame(localId = 4)
+
+        insertParticipant(playerId = player.rowId, finishingPosition = 1, gameId = winningGameId)
+        insertParticipant(playerId = player.rowId, finishingPosition = 2, gameId = secondGameId)
+        insertParticipant(playerId = player.rowId, finishingPosition = 3, gameId = thirdGameId)
+        insertParticipant(playerId = player.rowId, finishingPosition = -1, gameId = unfinishedGameId)
+
+        val ipp = IncludedPlayerParameters()
+        ipp.finishingPositions = listOf(1, 3)
+
+        val rp = ReportParameters()
+        rp.hmIncludedPlayerToParms[player] = ipp
+
+        val results = runReport(rp)
+        results shouldHaveSize 2
+
+        results.map{ it.localId }.shouldContainExactlyInAnyOrder(1, 2)
+    }
+}
