5407b1f46a8bd78bb230c32a360c69a2baae64e2
alexburlton <alexburlton@hotmail.com>
2019-04-25 21:08:54 +0100

    Start looking at testing achievements

--- Darts/src/burlton/dartzee/test/achievements/TestAchievementGolfBestGame.kt
+++ Darts/src/burlton/dartzee/test/achievements/TestAchievementGolfBestGame.kt
@@ -1,8 +1,20 @@
 package burlton.dartzee.test.achievements
 
 import burlton.dartzee.code.achievements.AchievementGolfBestGame
+import burlton.dartzee.code.db.GAME_TYPE_GOLF
+import io.kotlintest.shouldBe
+import org.junit.Test
 
 class TestAchievementGolfBestGame: AbstractAchievementTest<AchievementGolfBestGame>()
 {
     override fun factoryAchievement() = AchievementGolfBestGame()
+
+    @Test
+    fun `Should only count 18-hole games`()
+    {
+        val achievement = factoryAchievement()
+
+        achievement.gameType shouldBe GAME_TYPE_GOLF
+        achievement.gameParams shouldBe "18"
+    }
 }
