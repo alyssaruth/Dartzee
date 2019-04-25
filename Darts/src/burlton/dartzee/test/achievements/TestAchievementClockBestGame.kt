4d98a24bbd385f5f75dcfd729c954d3b1f204ab6
alexburlton <alexburlton@hotmail.com>
2019-04-25 21:08:54 +0100

    Start looking at testing achievements

--- Darts/src/burlton/dartzee/test/achievements/TestAchievementClockBestGame.kt
+++ Darts/src/burlton/dartzee/test/achievements/TestAchievementClockBestGame.kt
@@ -1,8 +1,21 @@
 package burlton.dartzee.test.achievements
 
 import burlton.dartzee.code.achievements.AchievementClockBestGame
+import burlton.dartzee.code.db.CLOCK_TYPE_STANDARD
+import burlton.dartzee.code.db.GAME_TYPE_ROUND_THE_CLOCK
+import io.kotlintest.shouldBe
+import org.junit.Test
 
 class TestAchievementClockBestGame: AbstractAchievementTest<AchievementClockBestGame>()
 {
     override fun factoryAchievement() = AchievementClockBestGame()
+
+    @Test
+    fun `Should only count standard games of RTC`()
+    {
+        val achievement = factoryAchievement()
+
+        achievement.gameType shouldBe GAME_TYPE_ROUND_THE_CLOCK
+        achievement.gameParams shouldBe CLOCK_TYPE_STANDARD
+    }
 }
--- Darts/src/burlton/dartzee/test/achievements/TestAchievementX01BestGame.kt
+++ Darts/src/burlton/dartzee/test/achievements/TestAchievementX01BestGame.kt
@@ -1,8 +1,21 @@
 package burlton.dartzee.test.achievements
 
 import burlton.dartzee.code.achievements.AchievementX01BestGame
+import burlton.dartzee.code.db.GAME_TYPE_X01
+import io.kotlintest.shouldBe
+import org.junit.Test
 
 class TestAchievementX01BestGame: AbstractAchievementTest<AchievementX01BestGame>()
 {
     override fun factoryAchievement() = AchievementX01BestGame()
+
+    @Test
+    fun `Should only count games of 501`()
+    {
+        val achievement = factoryAchievement()
+
+        achievement.gameType shouldBe GAME_TYPE_X01
+        achievement.gameParams shouldBe "501"
+    }
+
 }
