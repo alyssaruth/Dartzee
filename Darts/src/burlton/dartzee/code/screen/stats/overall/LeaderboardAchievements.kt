eff410f9379e1009631f87c50ec3ec3db0f12b4d
alexburlton <alexburlton@hotmail.com>
2019-04-29 12:54:27 +0100

    Combine achievement leaderboards using dummy achievement

--- Darts/src/burlton/dartzee/code/screen/stats/overall/LeaderboardAchievements.kt
+++ Darts/src/burlton/dartzee/code/screen/stats/overall/LeaderboardAchievements.kt
@@ -1,21 +1,25 @@
 package burlton.dartzee.code.screen.stats.overall
 
-import burlton.dartzee.code.achievements.getAchievementMaximum
-import burlton.dartzee.code.achievements.getPlayerAchievementScore
+import burlton.dartzee.code.achievements.AbstractAchievement
+import burlton.dartzee.code.achievements.DummyAchievementTotal
+import burlton.dartzee.code.achievements.getAllAchievements
 import burlton.dartzee.code.bean.PlayerTypeFilterPanel
 import burlton.dartzee.code.bean.ScrollTableAchievements
-import burlton.dartzee.code.db.AchievementEntity
 import burlton.dartzee.code.db.PlayerEntity
-import burlton.dartzee.code.utils.DartsColour
 import burlton.desktopcore.code.util.TableUtil
 import java.awt.BorderLayout
 import java.awt.Color
-import javax.swing.JPanel
+import java.awt.event.ActionEvent
+import java.util.*
+import javax.swing.*
 
-class LeaderboardAchievements : AbstractLeaderboard()
+class LeaderboardAchievements: AbstractLeaderboard()
 {
     private val table = ScrollTableAchievements()
     private val panelFilters = JPanel()
+
+    private val cbSpecificAchievement = JCheckBox("Achievement")
+    private val comboBox = JComboBox<AbstractAchievement>()
     private val playerFilterPanel = PlayerTypeFilterPanel()
 
     init
@@ -23,58 +27,106 @@ class LeaderboardAchievements : AbstractLeaderboard()
         layout = BorderLayout(0, 0)
         table.setRowHeight(23)
         add(table)
-
         add(panelFilters, BorderLayout.NORTH)
+        panelFilters.add(cbSpecificAchievement)
+        panelFilters.add(comboBox)
+        panelFilters.add(Box.createHorizontalStrut(20))
         panelFilters.add(playerFilterPanel)
+        comboBox.isEnabled = false
 
+        initComboBox()
+
+        comboBox.addActionListener(this)
         playerFilterPanel.addActionListener(this)
+        cbSpecificAchievement.addActionListener(this)
+    }
+
+    private fun initComboBox()
+    {
+        val achievements = Vector(getAllAchievements())
+        comboBox.model = DefaultComboBoxModel(achievements)
     }
 
     override fun getTabName() = "Achievements"
 
     override fun buildTable()
     {
-        val achievementRows = AchievementEntity().retrieveEntities("")
+        val achievement = getSelectedAchievement()
+        val achievementRows = achievement.retrieveAllRows()
 
         val model = TableUtil.DefaultModel()
         model.addColumn("")
         model.addColumn("Player")
-        model.addColumn("Achievements")
+        model.addColumn("Score")
 
         val players = PlayerEntity.retrievePlayers(playerFilterPanel.whereSql, false)
-        players.forEach{
-            val score = getPlayerAchievementScore(achievementRows, it)
-            val row = arrayOf(it.getFlag(), it, score)
+        players.forEach{ p ->
+            val myRows = achievementRows.filter { it.playerId == p.rowId }
 
+            val myAchievement = achievement.javaClass.newInstance()
+            myAchievement.initialiseFromDb(myRows, p)
+
+            if (myAchievement.isDecreasing() && myAchievement.attainedValue == -1)
+            {
+                myAchievement.attainedValue = Integer.MAX_VALUE
+            }
+
+            val row = arrayOf(p.getFlag(), p, myAchievement)
             model.addRow(row)
         }
 
         table.model = model
         table.setColumnWidths("25;200")
-        table.sortBy(2, true)
+        table.setComparator(2, compareBy<AbstractAchievement>{ it.attainedValue })
+        table.sortBy(2, !achievement.isDecreasing())
 
-        val renderer = OverallAchievementRenderer()
-        renderer.maximum = getAchievementMaximum()
+        val renderer = AchievementProgressBarRenderer()
+        renderer.minimum = 0
+        if (achievement.isDecreasing())
+        {
+            renderer.maximum = achievement.redThreshold - achievement.maxValue
+        }
+        else
+        {
+            renderer.maximum = achievement.maxValue
+        }
+
         table.getColumn(2).cellRenderer = renderer
     }
 
-    class OverallAchievementRenderer: AbstractProgressBarRenderer()
-    {
-        override fun getScoreForValue(value: Any?) = value as Int
+    private fun getSelectedAchievement() = if (cbSpecificAchievement.isSelected) comboBox.getItemAt(comboBox.selectedIndex) else DummyAchievementTotal()
 
+    override fun actionPerformed(e: ActionEvent?)
+    {
+        super.actionPerformed(e)
+
+        comboBox.isEnabled = cbSpecificAchievement.isSelected
+    }
+
+    class AchievementProgressBarRenderer: AbstractProgressBarRenderer()
+    {
         override fun getColorForValue(value: Any?): Color
         {
-            val score = value as Int
-            return when(score)
+            return (value as AbstractAchievement).getColor(false)
+        }
+
+        override fun getScoreForValue(value: Any?): Int
+        {
+            val achievement = (value as AbstractAchievement)
+
+            if (achievement.isDecreasing())
             {
-                in 5*maximum/6 until Int.MAX_VALUE -> Color.MAGENTA
-                in 4*maximum/6 until 5*maximum/6 -> Color.CYAN
-                in 3*maximum/6 until 4*maximum/6 -> Color.GREEN
-                in 2*maximum/6 until 3*maximum/6 -> Color.YELLOW
-                in maximum/6 until 2*maximum/6 -> DartsColour.COLOUR_ACHIEVEMENT_ORANGE
-                in 1 until maximum/6 -> Color.RED
-                else -> Color.GRAY
+                return Math.max(achievement.redThreshold - achievement.attainedValue, 0)
             }
+
+            return achievement.attainedValue
+        }
+
+        override fun getScoreDescForValue(value: Any?): String
+        {
+            val achievement = value as AbstractAchievement
+            val attainedValueDesc = if (achievement.attainedValue == -1 || achievement.attainedValue == Integer.MAX_VALUE) "-" else "${achievement.attainedValue}"
+            return "$attainedValueDesc/${achievement.maxValue}"
         }
     }
 }
