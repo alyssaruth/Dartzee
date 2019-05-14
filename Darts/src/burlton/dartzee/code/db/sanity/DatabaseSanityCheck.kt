22c8f03f153449a56620e3f798167592448c4f35
alexburlton <alexburlton@hotmail.com>
2019-05-14 13:21:47 +0100

    Progress bar for running sanity checks

--- Darts/src/burlton/dartzee/code/db/sanity/DatabaseSanityCheck.kt
+++ Darts/src/burlton/dartzee/code/db/sanity/DatabaseSanityCheck.kt
@@ -1,9 +1,11 @@
 package burlton.dartzee.code.db.sanity
 
 import burlton.core.code.util.Debug
+import burlton.core.code.util.runOnEventThread
 import burlton.dartzee.code.screen.ScreenCache
 import burlton.dartzee.code.utils.DartsDatabaseUtil
 import burlton.desktopcore.code.bean.ScrollTableButton
+import burlton.desktopcore.code.screen.ProgressDialog
 import burlton.desktopcore.code.screen.TableModelDialog
 import burlton.desktopcore.code.util.DialogUtil
 import burlton.desktopcore.code.util.TableUtil.DefaultModel
@@ -44,11 +46,35 @@ object DatabaseSanityCheck
 
         sanityErrors.clear()
 
+        runAllChecks()
+    }
+
+    private fun runAllChecks()
+    {
+        val r = Runnable { runChecksInOtherThread(getAllSanityChecks())}
+        val t = Thread(r, "Sanity checks")
+        t.start()
+    }
+
+    private fun runChecksInOtherThread(checks: List<AbstractSanityCheck>)
+    {
+        val dlg = ProgressDialog.factory("Running Sanity Check", "checks remaining", checks.size)
+        dlg.setVisibleLater()
+
         getAllSanityChecks().forEach{
             val results = it.runCheck()
             sanityErrors.addAll(results)
+
+            dlg.incrementProgressLater()
         }
 
+        dlg.disposeLater()
+
+        runOnEventThread { sanityCheckComplete() }
+    }
+
+    private fun sanityCheckComplete()
+    {
         val tm = buildResultsModel()
         if (tm.rowCount > 0)
         {
