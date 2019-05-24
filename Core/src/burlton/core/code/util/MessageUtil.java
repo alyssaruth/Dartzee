cf0e7ef4838f5f2c74d833d74a47e7261636a61e
alexburlton <alexburlton@hotmail.com>
2019-05-24 20:32:11 +0100

    Delete AppUpdater module Tidy up some unused code in AbstractClient/MessageSender/MessageUtil Move new update.bat asset into Darts module Pull in a new UpdateChecker.kt written for the new mechanism Pull in unirest dependencies, although may move to khttp if this is more lightweight

--- Core/src/burlton/core/code/util/MessageUtil.java
+++ Core/src/burlton/core/code/util/MessageUtil.java
@@ -1,6 +1,7 @@
 
 package burlton.core.code.util;
 
+import javax.crypto.SecretKey;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -10,31 +11,18 @@ import java.net.UnknownHostException;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.ArrayList;
 import java.util.Random;
 
-import javax.crypto.SecretKey;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
 public class MessageUtil implements OnlineConstants
-{	
-	public static final String LIVE_IP = "54.149.26.201"; //Live
-	public static final String LAPTOP_IP = "82.3.206.177"; //Laptop (in Leeds)
-	public static final String LOCAL_IP = "localhost";
-	
-	public static final String SERVER_IP = LIVE_IP;
+{
+	public static final String SERVER_IP = "54.149.26.201"; //Live
 	
 	private static final Random RAND = new Random();
 	
 	public static int millisDelay = 0;
 	public static PublicKey publicKey = null;
 	public static SecretKey symmetricKey = null;
-	public static SecretKey tempSymmetricKey = null; //Used to cache a key
-	
-	private static boolean shuttingDown = false;
+
 	private static int cachedPortNumber = -1;
 	
 	public static void generatePublicKey()
@@ -56,48 +44,7 @@ public class MessageUtil implements OnlineConstants
 			Debug.stackTrace(e, "Unable to read public key - won't be able to communicate with Server.");
 		} 
 	}
-	
-	public static void sendMessage(Document message, int millis)
-	{
-		sendMessage(message, millis, 5);
-	}
-	public static void sendMessage(Document message, int millis, int retries)
-	{
-		String messageString = XmlUtil.getStringFromDocument(message);
-		sendMessage(messageString, millis, retries);
-	}
-	public static void sendMessage(String messageString, int millis)
-	{
-		sendMessage(messageString, millis, 5);
-	}
-	public static void sendMessage(String messageString, int millis, int retries)
-	{
-		MessageSenderParams params = new MessageSenderParams(messageString, millis, retries);
-		sendMessage(params, true);
-	}
-	public static void sendMessage(MessageSenderParams message, boolean encrypt)
-	{
-		String messageString = message.getMessageString();
-		String encryptedMessageString = messageString;
-		if (encrypt)
-		{
-			encryptedMessageString = EncryptionUtil.encrypt(messageString, symmetricKey);
-		}
-		
-		message.setEncryptedMessageString(encryptedMessageString);
-		
-		AbstractClient.getInstance().sendAsyncInSingleThread(message);
-	}
-	
-	public static boolean serverIsShuttingDown()
-	{
-		return shuttingDown;
-	}
-	public static void setShuttingDown(boolean serverShuttingDown)
-	{
-		shuttingDown = serverShuttingDown;
-	}
-	
+
 	public static int getRandomPortNumber()
 	{
 		synchronized (RAND)
@@ -112,44 +59,7 @@ public class MessageUtil implements OnlineConstants
 			return cachedPortNumber;
 		}
 	}
-	
-	public static boolean changeCachedPort(Element rootElement)
-	{
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		NodeList usedPorts = rootElement.getElementsByTagName("UsedPort");
-		int size = usedPorts.getLength();
-		for (int i=0; i<size; i++)
-		{
-			Element portElement = (Element)usedPorts.item(i);
-			int port = XmlUtil.getAttributeInt(portElement, "PortNumber");
-			list.add(port);
-		}
-		
-		int portsToChoose = SERVER_PORT_NUMBER_UPPER_BOUND - SERVER_PORT_NUMBER_LOWER_BOUND;
-		if (list.size() >= portsToChoose)
-		{
-			cachedPortNumber = -1;
-			return false;
-		}
-		
-		Random rand = new Random();
-		cachedPortNumber = SERVER_PORT_NUMBER_LOWER_BOUND + rand.nextInt(portsToChoose);
-		while (list.contains(cachedPortNumber))
-		{
-			cachedPortNumber = SERVER_PORT_NUMBER_LOWER_BOUND + rand.nextInt(portsToChoose);
-		}
-		
-		return true;
-	}
-	
-	/*public static int getRandomLoadTestPortNumber()
-	{
-		Random rand = new Random();
-		int portsToChoose = LOAD_TEST_PORT_NUMBER_UPPER_BOUND - LOAD_TEST_PORT_NUMBER_LOWER_BOUND;
-		return LOAD_TEST_PORT_NUMBER_LOWER_BOUND + rand.nextInt(portsToChoose);
-	}*/
-	
+
 	public static InetAddress factoryInetAddress(String ipAddress)
 	{
 		InetAddress address = null;
