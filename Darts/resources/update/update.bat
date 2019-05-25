a428d751a1c56fbb7a71f5fe1e16a9af85394b2a
alexburlton <alexburlton@hotmail.com>
2019-05-25 12:12:12 +0100

    It basically works!

--- Darts/resources/update/update.bat
+++ Darts/resources/update/update.bat
@@ -7,11 +7,11 @@ REM %4 = assetId
 
 echo Performing download of %1 bytes (Version %2)
 
-curl -LJO -H "Accept: application/octet-stream" https://api.github.com/repos/alexburlton/DartzeeRelease/releases/assets/12806914
+curl -LJO -H "Accept: application/octet-stream" https://api.github.com/repos/alexburlton/DartzeeRelease/releases/assets/%4
 
 ren Dartzee.jar Dartzee_OLD.jar
 ren %3 Dartzee.jar
 del Dartzee_OLD.jar
 
-start javaw -jar %3 justUpdated
+start javaw -jar Dartzee.jar justUpdated
 exit
