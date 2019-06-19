b4619058f0582b785ad58b213da3438100d3001a
alexburlton <alexburlton@hotmail.com>
2019-06-19 08:09:52 +0100

    Convert DartsModelNormalDistribution -> Kotlin

---
+++ Darts/src/burlton/dartzee/test/ai/TestDartsModelNormalDistribution.kt
@@ -0,0 +1,18 @@
+package burlton.dartzee.test.ai
+
+import burlton.dartzee.test.helper.AbstractDartsTest
+
+class TestDartsModelNormalDistribution: AbstractDartsTest()
+{
+    /**
+     * Read / write XML etc
+     */
+
+    /**
+     * Populate
+     */
+
+    /**
+     * Actual behaviour by mocking out the normal dists
+     */
+}
