d7191c5d06e09bbe1f338af3a01b2c7170bb60e2
alexburlton <alexburlton@hotmail.com>
2019-08-06 18:08:05 +0100

    Add specific test around special field type

--- Darts/src/burlton/dartzee/test/db/TestDartzeeRuleEntity.kt
+++ Darts/src/burlton/dartzee/test/db/TestDartzeeRuleEntity.kt
@@ -1,8 +1,26 @@
 package burlton.dartzee.test.db
 
+import burlton.dartzee.code.dartzee.DartzeeDartRuleEven
 import burlton.dartzee.code.db.DartzeeRuleEntity
+import io.kotlintest.shouldBe
+import org.junit.Test
 
 class TestDartzeeRuleEntity: AbstractEntityTest<DartzeeRuleEntity>()
 {
     override fun factoryDao() = DartzeeRuleEntity()
+
+    @Test
+    fun `Should support NULL dartzee rules`()
+    {
+        val entity = DartzeeRuleEntity()
+        val rowId = entity.assignRowId()
+        entity.dart1Rule = DartzeeDartRuleEven()
+        entity.dart2Rule = null
+
+        entity.saveToDatabase()
+
+        val reretrievedEntity = DartzeeRuleEntity().retrieveForId(rowId)!!
+        reretrievedEntity.dart1Rule shouldBe DartzeeDartRuleEven()
+        reretrievedEntity.dart2Rule shouldBe null
+    }
 }
