e623ebccb2a0527e9eb04fedc2ccfb9a3c4d17cb
alexburlton <alexburlton@hotmail.com>
2019-08-21 08:55:41 +0100

    Getting there I guess...

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleVerificationPanel.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleVerificationPanel.kt
@@ -1,28 +1,99 @@
 package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.`object`.Dart
+import burlton.dartzee.code.bean.DartzeeDartResult
+import burlton.dartzee.code.db.DartzeeRuleEntity
 import burlton.dartzee.code.listener.DartboardListener
 import burlton.dartzee.code.screen.Dartboard
+import net.miginfocom.swing.MigLayout
 import java.awt.BorderLayout
 import java.awt.Dimension
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.ImageIcon
+import javax.swing.JButton
 import javax.swing.JPanel
+import javax.swing.border.TitledBorder
 
-class DartzeeRuleVerificationPanel: JPanel(), DartboardListener
+class DartzeeRuleVerificationPanel: JPanel(), DartboardListener, ActionListener
 {
     private val dartboard = Dartboard(300, 300)
     private val dartsThrown = mutableListOf<Dart>()
+    private val btnReset = JButton()
+    private val panelSouth = JPanel()
+    private val panelDartSummary = JPanel()
+    private val panelDartOne = DartzeeDartResult()
+    private val panelDartTwo = DartzeeDartResult()
+    private val panelDartThree = DartzeeDartResult()
+
+    private var dartzeeRule = DartzeeRuleEntity()
 
     init
     {
         layout = BorderLayout(0, 0)
-        preferredSize = Dimension(300, 300)
+        preferredSize = Dimension(330, 300)
+        border = TitledBorder("Test Rule")
 
+        dartboard.renderScoreLabels = true
         dartboard.paintDartboard()
+        dartboard.addDartboardListener(this)
         add(dartboard, BorderLayout.CENTER)
+        add(panelSouth, BorderLayout.SOUTH)
+
+        panelSouth.layout = BorderLayout(0, 0)
+
+        panelSouth.add(btnReset, BorderLayout.WEST)
+
+        panelDartSummary.layout = MigLayout("", "[]", "[]")
+        panelSouth.add(panelDartSummary, BorderLayout.CENTER)
+
+        panelDartSummary.add(panelDartOne, "cell 0 0")
+        panelDartSummary.add(panelDartTwo, "cell 0 1")
+        panelDartSummary.add(panelDartThree, "cell 0 2")
+
+        btnReset.preferredSize = Dimension(80, 80)
+        btnReset.icon = ImageIcon(javaClass.getResource("/buttons/Reset.png"))
+        btnReset.toolTipText = "Reset darts"
+
+        btnReset.addActionListener(this)
     }
+
+    fun updateRule(rule: DartzeeRuleEntity)
+    {
+        this.dartzeeRule = rule
+    }
+
 
     override fun dartThrown(dart: Dart)
     {
         dartsThrown.add(dart)
+
+        panelDartOne.update(dartsThrown[0])
+
+        if (dartsThrown.size > 1)
+        {
+            panelDartTwo.update(dartsThrown[1])
+        }
+
+        if (dartsThrown.size > 2)
+        {
+            panelDartThree.update(dartsThrown[2])
+        }
+
+        if (dartsThrown.size == 3)
+        {
+            dartboard.stopListening()
+        }
+    }
+
+    override fun actionPerformed(e: ActionEvent?)
+    {
+        dartsThrown.clear()
+        dartboard.clearDarts()
+        dartboard.ensureListening()
+
+        panelDartOne.reset()
+        panelDartTwo.reset()
+        panelDartThree.reset()
     }
 }
