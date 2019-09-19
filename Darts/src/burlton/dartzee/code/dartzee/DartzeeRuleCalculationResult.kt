c2237cc40f032c2876e2725bf3a65a8ae0cdb520
alexburlton <alexburlton@hotmail.com>
2019-09-19 19:11:43 +0100

    Split out CalculationResult, actually update difficulty based on success %

---
+++ Darts/src/burlton/dartzee/code/dartzee/DartzeeRuleCalculationResult.kt
@@ -0,0 +1,26 @@
+package burlton.dartzee.code.dartzee
+
+import burlton.core.code.util.MathsUtil
+import burlton.dartzee.code.`object`.DartboardSegment
+
+data class DartzeeRuleCalculationResult(val validSegments: List<DartboardSegment>,
+                                        val validCombinations: Int,
+                                        val allCombinations: Int,
+                                        val validCombinationProbability: Double,
+                                        val allCombinationsProbability: Double)
+{
+    private val percentage = MathsUtil.getPercentage(validCombinationProbability, allCombinationsProbability)
+
+    fun getCombinationsDesc() = "$validCombinations combinations (success%: $percentage%)"
+
+    fun getDifficultyDesc() = when
+    {
+        percentage == 0.0 -> "Impossible"
+        percentage > 40 -> "Very Easy"
+        percentage > 25 -> "Easy"
+        percentage > 10 -> "Moderate"
+        percentage > 5 -> "Hard"
+        percentage > 1 -> "Very Hard"
+        else -> "Insane"
+    }
+}
