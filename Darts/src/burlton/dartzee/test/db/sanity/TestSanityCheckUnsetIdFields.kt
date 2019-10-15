185206964e51a7c6f15ad34203c391031eb43fce
Alex Burlton <alexburlton@hotmail.com>
2019-10-15 13:26:15 +0100

    Finish off UnsetId sanity check tests

--- Darts/src/burlton/dartzee/test/db/sanity/TestSanityCheckUnsetIdFields.kt
+++ Darts/src/burlton/dartzee/test/db/sanity/TestSanityCheckUnsetIdFields.kt
@@ -1,10 +1,13 @@
 package burlton.dartzee.test.db.sanity
 
 import burlton.dartzee.code.db.GameEntity
+import burlton.dartzee.code.db.ParticipantEntity
 import burlton.dartzee.code.db.sanity.SanityCheckUnsetIdFields
 import burlton.dartzee.test.helper.AbstractDartsTest
 import burlton.dartzee.test.helper.insertGame
+import burlton.dartzee.test.helper.insertParticipant
 import io.kotlintest.matchers.collections.shouldBeEmpty
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
 import org.junit.Test
 
 class TestSanityCheckUnsetIdFields: AbstractDartsTest()
@@ -16,5 +19,15 @@ class TestSanityCheckUnsetIdFields: AbstractDartsTest()
 
         val results = SanityCheckUnsetIdFields(GameEntity()).runCheck()
         results.shouldBeEmpty()
+    }
+
+    @Test
+    fun `Should flag up unset ID fields`()
+    {
+        insertParticipant(gameId = "", playerId = "")
+
+        val results = SanityCheckUnsetIdFields(ParticipantEntity()).runCheck()
+        results.map { it.getDescription() }.shouldContainExactlyInAnyOrder("Participant rows where GameId is unset",
+            "Participant rows where PlayerId is unset")
     }
 }
