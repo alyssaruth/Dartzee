124bcc75d69042dc67fd8a53441d58cd4c2b5d43
alexburlton <alex.burlton@sonocent.com>
2019-10-16 09:14:49 +0100

    Start on ScrollTableOrdered tests

---
+++ Darts/src/burlton/dartzee/test/bean/TestScrollTableOrdered.kt
@@ -0,0 +1,57 @@
+package burlton.dartzee.test.bean
+
+import burlton.dartzee.test.helper.AbstractDartsTest
+import org.junit.Test
+
+class TestScrollTableOrdered: AbstractDartsTest()
+{
+    //TODO - fill in tests
+
+    @Test
+    fun `should move rows up`()
+    {
+
+    }
+
+    @Test
+    fun `Should do nothing if trying to move the first row up`()
+    {
+
+    }
+
+    @Test
+    fun `should move rows down`()
+    {
+
+    }
+
+    @Test
+    fun `Should do nothing if trying to move last row down`()
+    {
+
+    }
+
+    @Test
+    fun `Should do nothing if trying to move a row with no selection`()
+    {
+
+    }
+
+    @Test
+    fun `Should support programatically sorting the rows by something`()
+    {
+
+    }
+
+    @Test
+    fun `Should grab all the rows`()
+    {
+
+    }
+
+    @Test
+    fun `Should scramble the row order`()
+    {
+
+    }
+}
