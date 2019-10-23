8ab8c272ea0db4eaf14ca846a41a081263324538
alexburlton <alex.burlton@sonocent.com>
2019-10-23 08:42:16 +0100

    Mm... struggling for motivation today...

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeTemplateSetupScreen.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeTemplateSetupScreen.kt
@@ -3,12 +3,19 @@ package burlton.dartzee.code.screen.dartzee
 import burlton.dartzee.code.screen.EmbeddedScreen
 import burlton.dartzee.code.screen.ScreenCache
 import burlton.dartzee.code.screen.UtilitiesScreen
+import burlton.desktopcore.code.bean.ScrollTable
+import java.awt.BorderLayout
+import javax.swing.JPanel
 
 class DartzeeTemplateSetupScreen: EmbeddedScreen()
 {
+    private val scrollTable = ScrollTable()
+    private val panelEast = JPanel()
+
     override fun initialise()
     {
-
+        add(scrollTable)
+        add(panelEast, BorderLayout.EAST)
     }
 
     override fun getScreenName() = "Dartzee Templates"
