2c09c6b567f2555e37c43d2b0baae03a74e97589
Alex Burlton <alexburlton@hotmail.com>
2019-10-23 08:33:44 +0100

    Fix a couple of warnings in DartsApp

--- Darts/src/burlton/dartzee/code/screen/DartsApp.kt
+++ Darts/src/burlton/dartzee/code/screen/DartsApp.kt
@@ -30,8 +30,7 @@ private const val CMD_GUID = "guid"
 
 class DartsApp(commandBar: CheatBar) : AbstractDevScreen(commandBar), WindowListener
 {
-    var currentScreen: EmbeddedScreen? = null
-        private set
+    private var currentScreen: EmbeddedScreen? = null
 
     init
     {
@@ -86,7 +85,7 @@ class DartsApp(commandBar: CheatBar) : AbstractDevScreen(commandBar), WindowList
 
     private fun addConsoleShortcut()
     {
-        val triggerStroke = KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK)
+        val triggerStroke = KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK)
         val content = contentPane as JPanel
 
         val inputMap = content.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
