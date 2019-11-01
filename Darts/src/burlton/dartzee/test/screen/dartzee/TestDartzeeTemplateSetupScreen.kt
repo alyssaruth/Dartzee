e6c4cff80bd727e9b317b47668fb1bc2738c8899
alexburlton <alex.burlton@sonocent.com>
2019-11-01 13:52:58 +0000

    Start adding more tests for the TemplateSetupScreen

--- Darts/src/burlton/dartzee/test/screen/dartzee/TestDartzeeTemplateSetupScreen.kt
+++ Darts/src/burlton/dartzee/test/screen/dartzee/TestDartzeeTemplateSetupScreen.kt
@@ -1,12 +1,16 @@
 package burlton.dartzee.test.screen.dartzee
 
+import burlton.dartzee.code.db.DARTZEE_TEMPLATE
 import burlton.dartzee.code.db.GAME_TYPE_DARTZEE
 import burlton.dartzee.code.screen.dartzee.DartzeeTemplateSetupScreen
 import burlton.dartzee.test.helper.AbstractDartsTest
+import burlton.dartzee.test.helper.getCountFromTable
 import burlton.dartzee.test.helper.insertDartzeeTemplate
 import burlton.dartzee.test.helper.insertGame
+import io.kotlintest.matchers.collections.shouldContainExactly
 import io.kotlintest.shouldBe
 import org.junit.Test
+import javax.swing.JOptionPane
 
 class TestDartzeeTemplateSetupScreen: AbstractDartsTest()
 {
@@ -33,6 +37,61 @@ class TestDartzeeTemplateSetupScreen: AbstractDartsTest()
         scrn.initialise()
 
         scrn.getGameCount(0) shouldBe 2
+    }
+
+    @Test
+    fun `Should toggle copy & remove buttons based on whether a row is selected`()
+    {
+        insertDartzeeTemplate()
+
+        val scrn = DartzeeTemplateSetupScreen()
+        scrn.initialise()
+
+        scrn.btnCopy.isEnabled shouldBe false
+        scrn.btnDelete.isEnabled shouldBe false
+
+        scrn.scrollTable.selectRow(0)
+        scrn.btnCopy.isEnabled shouldBe true
+        scrn.btnDelete.isEnabled shouldBe true
+
+        scrn.scrollTable.selectRow(-1)
+        scrn.btnCopy.isEnabled shouldBe false
+        scrn.btnDelete.isEnabled shouldBe false
+    }
+
+    @Test
+    fun `Should leave template alone if delete is cancelled`()
+    {
+        insertDartzeeTemplate(name = "ABC")
+        dialogFactory.questionOption = JOptionPane.NO_OPTION
+
+        val scrn = DartzeeTemplateSetupScreen()
+        scrn.initialise()
+
+        scrn.scrollTable.selectRow(0)
+        scrn.btnDelete.doClick()
+
+        dialogFactory.questionsShown.shouldContainExactly("Are you sure you want to delete the ABC Template?")
+        scrn.scrollTable.rowCount shouldBe 1
+        getCountFromTable(DARTZEE_TEMPLATE) shouldBe 1
+    }
+
+    @Test
+    fun `Should delete a template on confirmation`()
+    {
+        insertDartzeeTemplate(name = "ABC")
+        dialogFactory.questionOption = JOptionPane.YES_OPTION
+
+        val scrn = DartzeeTemplateSetupScreen()
+        scrn.initialise()
+
+        scrn.scrollTable.selectRow(0)
+        scrn.btnDelete.doClick()
+
+        dialogFactory.questionsShown.shouldContainExactly("Are you sure you want to delete the ABC Template?")
+
+        scrn.scrollTable.rowCount shouldBe 0
+        getCountFromTable(DARTZEE_TEMPLATE) shouldBe 0
     }
 
     private fun DartzeeTemplateSetupScreen.getGameCount(row: Int): Int
