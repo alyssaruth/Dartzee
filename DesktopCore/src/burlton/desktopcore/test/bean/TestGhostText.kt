422a7d421f3a3ea23dc425aeeb885913edf26619
alexburlton <alexburlton@hotmail.com>
2019-11-06 22:44:30 +0000

    Add GhostText tests

---
+++ DesktopCore/src/burlton/desktopcore/test/bean/TestGhostText.kt
@@ -0,0 +1,46 @@
+package burlton.desktopcore.test.bean
+
+import burlton.desktopcore.code.bean.GhostText
+import burlton.desktopcore.test.helpers.AbstractDesktopTest
+import io.kotlintest.shouldBe
+import org.junit.Test
+import java.awt.Font
+import javax.swing.JTextField
+import javax.swing.SwingConstants.LEADING
+
+class TestGhostText: AbstractDesktopTest()
+{
+    @Test
+    fun `Should construct with the right values`()
+    {
+        val label = JTextField()
+        val font = Font("Tahoma", Font.BOLD, 25)
+        label.font = font
+
+        val ghostText = GhostText("Foo", label)
+        ghostText.font shouldBe font
+        ghostText.text shouldBe "Foo"
+        ghostText.foreground.alpha shouldBe 127
+        ghostText.horizontalAlignment shouldBe LEADING
+    }
+
+    @Test
+    fun `Should be visible when text field has no text`()
+    {
+        val tf = JTextField()
+
+        val ghostText = GhostText("Foo", tf)
+        ghostText.isVisible shouldBe true
+    }
+
+    @Test
+    fun `Should not be visible when text is entered`()
+    {
+        val tf = JTextField()
+
+        val ghostText = GhostText("Foo", tf)
+        tf.document.insertString(0, "some text", null)
+
+        ghostText.isVisible shouldBe false
+    }
+}
