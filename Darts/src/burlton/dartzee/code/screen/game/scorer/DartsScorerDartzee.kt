f295f111ef8280dee25d4622ef34a916cab5af63
Alex Burlton <alexburlton@hotmail.com>
2019-11-11 12:21:40 +0000

    Start to flesh out Dartzee scorer

--- Darts/src/burlton/dartzee/code/screen/game/scorer/DartsScorerDartzee.kt
+++ Darts/src/burlton/dartzee/code/screen/game/scorer/DartsScorerDartzee.kt
@@ -1,16 +1,21 @@
 package burlton.dartzee.code.screen.game.scorer
 
+private const val RULE_COLUMN = 3
+
 class DartsScorerDartzee: DartsScorer()
 {
     override fun getTotalScore() = 5
 
-    override fun rowIsComplete(rowNumber: Int) = true
+    override fun rowIsComplete(rowNumber: Int) = model.getValueAt(rowNumber, RULE_COLUMN) != null
 
     override fun getNumberOfColumns() = 5
 
     override fun initImpl(gameParams: String?)
     {
-        //Fuck knows
+        for (i in 0 until RULE_COLUMN)
+        {
+            tableScores.getColumn(i).cellRenderer = DartRenderer()
+        }
     }
 
 }
