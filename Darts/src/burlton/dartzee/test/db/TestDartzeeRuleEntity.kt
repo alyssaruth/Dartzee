ba9d977a3f4d38366c102ec35875312250769a22
alexburlton <alex.burlton@sonocent.com>
2019-11-12 13:23:28 +0000

    Tests for other DartzeeRuleEntity methods

--- Darts/src/burlton/dartzee/test/db/TestDartzeeRuleEntity.kt
+++ Darts/src/burlton/dartzee/test/db/TestDartzeeRuleEntity.kt
@@ -4,10 +4,12 @@ import burlton.dartzee.code.dartzee.dart.DartzeeDartRuleEven
 import burlton.dartzee.code.dartzee.dart.DartzeeDartRuleOdd
 import burlton.dartzee.code.dartzee.dart.DartzeeDartRuleOuter
 import burlton.dartzee.code.dartzee.total.DartzeeTotalRuleEven
+import burlton.dartzee.code.db.DARTZEE_TEMPLATE
 import burlton.dartzee.code.db.DartzeeRuleEntity
 import burlton.dartzee.test.doubleNineteen
-import burlton.dartzee.test.helper.makeDartzeeRuleCalculationResult
+import burlton.dartzee.test.helper.*
 import io.kotlintest.matchers.collections.shouldContainExactly
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotlintest.matchers.types.shouldBeInstanceOf
 import io.kotlintest.shouldBe
 import org.junit.Test
@@ -56,6 +58,62 @@ class TestDartzeeRuleEntity: AbstractEntityTest<DartzeeRuleEntity>()
 
         val newCalcResult = dto.calculationResult!!
         newCalcResult.validSegments.shouldContainExactly(doubleNineteen)
+    }
+
+    @Test
+    fun `Should retrieve rows for a template, sorted by ordinal`()
+    {
+        val templateA = insertDartzeeTemplate(name = "Template A")
+        val templateB = insertDartzeeTemplate(name = "Template B")
+
+        val ruleA3 = insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 3)
+        val ruleA1 = insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 1)
+        val ruleA2 = insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 2)
+
+        insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateB.rowId, ordinal = 1)
+        insertDartzeeRule(entityName = "Game", entityId = templateA.rowId, ordinal = 4)
+
+        val rules = DartzeeRuleEntity().retrieveForTemplate(templateA.rowId)
+
+        rules.map{ it.rowId }.shouldContainExactly(ruleA1.rowId, ruleA2.rowId, ruleA3.rowId)
+    }
+
+    @Test
+    fun `Should delete rows for a template`()
+    {
+        val templateA = insertDartzeeTemplate(name = "Template A")
+        val templateB = insertDartzeeTemplate(name = "Template B")
+
+        insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 3)
+        insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 1)
+        insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateA.rowId, ordinal = 2)
+
+        val ruleB1 = insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = templateB.rowId, ordinal = 1)
+        val gameRule = insertDartzeeRule(entityName = "Game", entityId = templateA.rowId, ordinal = 4)
+
+        DartzeeRuleEntity().deleteForTemplate(templateA.rowId)
+
+        val remainingRules = DartzeeRuleEntity().retrieveEntities()
+
+        remainingRules.map { it.rowId }.shouldContainExactlyInAnyOrder(ruleB1.rowId, gameRule.rowId)
+    }
+
+    @Test
+    fun `Should retrieve rules for a game, sorted by ordinal`()
+    {
+        val gameA = insertGame()
+        val gameB = insertGame()
+
+        val ruleA3 = insertDartzeeRule(entityName = "Game", entityId = gameA.rowId, ordinal = 3)
+        val ruleA1 = insertDartzeeRule(entityName = "Game", entityId = gameA.rowId, ordinal = 1)
+        val ruleA2 = insertDartzeeRule(entityName = "Game", entityId = gameA.rowId, ordinal = 2)
+
+        insertDartzeeRule(entityName = "Game", entityId = gameB.rowId, ordinal = 1)
+        insertDartzeeRule(entityName = DARTZEE_TEMPLATE, entityId = gameA.rowId, ordinal = 4)
+
+        val rules = DartzeeRuleEntity().retrieveForGame(gameA.rowId)
+
+        rules.map{ it.rowId }.shouldContainExactly(ruleA1.rowId, ruleA2.rowId, ruleA3.rowId)
     }
 
 }
