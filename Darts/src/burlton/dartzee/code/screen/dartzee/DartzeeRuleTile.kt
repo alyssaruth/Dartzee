b7ea1e8ef28359d6cc2288b48fc5cc5ea3732383
alexburlton <alex.burlton@sonocent.com>
2019-11-13 13:21:26 +0000

    Soft disable the buttons so colour rendering works

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTile.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTile.kt
@@ -1,8 +1,10 @@
 package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.dartzee.DartzeeRuleDto
+import burlton.dartzee.code.utils.DartsColour
 import java.awt.Color
 import java.awt.Dimension
+import javax.swing.DefaultButtonModel
 import javax.swing.JButton
 
 class DartzeeRuleTile(dto: DartzeeRuleDto, ruleNumber: Int): JButton()
@@ -10,22 +12,30 @@ class DartzeeRuleTile(dto: DartzeeRuleDto, ruleNumber: Int): JButton()
     init
     {
         preferredSize = Dimension(150, 80)
-        text = "<html><center>#$ruleNumber <br /><br /> ${dto.generateRuleDescription()}</center></html>"
+        text = "<html><center><b>#$ruleNumber <br /><br /> ${dto.generateRuleDescription()}</b></center></html>"
     }
 
     fun setResult(success: Boolean)
     {
-        isEnabled = false
+        model = SoftDisableButtonModel()
+        isFocusable = false
 
         if (success)
         {
             background = Color.GREEN
-            foreground = Color.GREEN
+            foreground = DartsColour.getProportionalColour(1.0, 1, 0.4, 0.5)
         }
         else
         {
             background = Color.RED
-            foreground = Color.RED
+            foreground = DartsColour.getProportionalColour(0.0, 1, 0.4, 0.5)
         }
+    }
+
+
+    class SoftDisableButtonModel : DefaultButtonModel()
+    {
+        override fun isPressed() = false
+        override fun isRollover() = false
     }
 }
