1414c6551a95c1697ab372d6b700b77861d81023
alexburlton <alex.burlton@sonocent.com>
2019-11-13 08:10:25 +0000

    Retrieve the DTOs in the panel

--- Darts/src/burlton/dartzee/code/screen/game/GamePanelDartzee.kt
+++ Darts/src/burlton/dartzee/code/screen/game/GamePanelDartzee.kt
@@ -3,6 +3,7 @@ package burlton.dartzee.code.screen.game
 import burlton.core.code.obj.HashMapList
 import burlton.dartzee.code.`object`.Dart
 import burlton.dartzee.code.ai.AbstractDartsModel
+import burlton.dartzee.code.db.DartzeeRoundResult
 import burlton.dartzee.code.db.DartzeeRuleEntity
 import burlton.dartzee.code.db.GameEntity
 import burlton.dartzee.code.screen.dartzee.DartboardRuleVerifier
@@ -14,15 +15,17 @@ class GamePanelDartzee(parent: AbstractDartsGameScreen, game: GameEntity) : Dart
 
     override fun factoryDartboard() = DartboardRuleVerifier()
 
-    override fun doAiTurn(model: AbstractDartsModel) {
+    override fun doAiTurn(model: AbstractDartsModel)
+    {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 
     override fun loadDartsForParticipant(playerNumber: Int, hmRoundToDarts: HashMapList<Int, Dart>, totalRounds: Int)
     {
-        //TODO - Change <Dart> -> <RoundResultWrapper>, which includes the Darts but also anything that happens to live in RoundDetail (new entity)
-        //Thinking that we can re-use RoundDetail for Golf when bunkers/hazards are introduced, to capture that these were hit in a round
+        val pt = hmPlayerNumberToParticipant[playerNumber]!!
+
+        val roundResults = DartzeeRoundResult().retrieveEntities("PlayerId = ${pt.playerId} AND ParticipantId = ${pt.rowId}")
     }
 
     override fun updateVariablesForNewRound() {
