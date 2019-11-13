c053561ca18db549fd7d1a804b461738277bba50
alexburlton <alex.burlton@sonocent.com>
2019-11-13 08:59:04 +0000

    Start trying to wire in different states

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTile.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTile.kt
@@ -1,6 +1,7 @@
 package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.dartzee.DartzeeRuleDto
+import java.awt.Color
 import java.awt.Dimension
 import javax.swing.JButton
 
@@ -10,5 +11,21 @@ class DartzeeRuleTile(dto: DartzeeRuleDto, ruleNumber: Int): JButton()
     {
         preferredSize = Dimension(150, 80)
         text = "<html><center>#$ruleNumber <br /><br /> ${dto.generateRuleDescription()}</center></html>"
+    }
+
+    fun setResult(success: Boolean)
+    {
+        isEnabled = false
+
+        if (success)
+        {
+            background = Color.GREEN
+            foreground = Color.GREEN
+        }
+        else
+        {
+            background = Color.RED
+            foreground = Color.RED
+        }
     }
 }
