2a155eeefd28726a6753efbdd57a80f70a003cf7
Alex Burlton <alexburlton@hotmail.com>
2019-11-13 08:59:04 +0000

    Start trying to wire in different states

--- Darts/src/burlton/dartzee/code/screen/TestScreen.kt
+++ Darts/src/burlton/dartzee/code/screen/TestScreen.kt
@@ -1,6 +1,7 @@
 package burlton.dartzee.code.screen
 
 import burlton.dartzee.code.bean.GameParamFilterPanelDartzee
+import burlton.dartzee.code.db.DartzeeRoundResult
 import burlton.dartzee.code.db.DartzeeRuleEntity
 import burlton.dartzee.code.screen.dartzee.DartzeeRuleCarousel
 import java.awt.BorderLayout
@@ -42,10 +43,24 @@ class TestScreen: EmbeddedScreen()
         val carousel = DartzeeRuleCarousel(rules)
         panelNorth.removeAll()
         panelNorth.add(carousel, BorderLayout.CENTER)
+        carousel.update(makeResults())
 
         ScreenCache.getMainScreen().pack()
         panelNorth.repaint()
         repaint()
+    }
+
+    private fun makeResults(): List<DartzeeRoundResult>
+    {
+        val result1 = DartzeeRoundResult()
+        result1.ruleNumber = 1
+        result1.success = false
+
+        val result2 = DartzeeRoundResult()
+        result2.ruleNumber = 3
+        result2.success = true
+
+        return listOf(result1, result2)
     }
 
     override fun initialise() { }
--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleCarousel.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleCarousel.kt
@@ -1,12 +1,23 @@
 package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.dartzee.DartzeeRuleDto
+import burlton.dartzee.code.db.DartzeeRoundResult
 import javax.swing.JPanel
 
 class DartzeeRuleCarousel(dtos: List<DartzeeRuleDto>): JPanel()
 {
+    private val tiles = dtos.mapIndexed { ix, rule -> DartzeeRuleTile(rule, ix + 1) }
+
     init
     {
-        dtos.forEachIndexed { ix, rule -> add(DartzeeRuleTile(rule, ix + 1)) }
+        tiles.forEach { add(it) }
+    }
+
+    fun update(results: List<DartzeeRoundResult>)
+    {
+        results.forEach {
+            val tile = tiles[it.ruleNumber - 1]
+            tile.setResult(it.success)
+        }
     }
 }
