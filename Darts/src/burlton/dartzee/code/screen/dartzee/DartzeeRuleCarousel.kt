4f175099f80f5e2adf7fc36e981a03de3ed83982
alexburlton <alex.burlton@sonocent.com>
2019-11-13 13:05:19 +0000

    Horizontal scroll, if necessary

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleCarousel.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleCarousel.kt
@@ -2,15 +2,30 @@ package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.dartzee.DartzeeRuleDto
 import burlton.dartzee.code.db.DartzeeRoundResult
+import java.awt.BorderLayout
+import java.awt.Dimension
 import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.ScrollPaneConstants
 
 class DartzeeRuleCarousel(dtos: List<DartzeeRuleDto>): JPanel()
 {
+    private val scrollPane = JScrollPane()
     private val tiles = dtos.mapIndexed { ix, rule -> DartzeeRuleTile(rule, ix + 1) }
 
     init
     {
-        tiles.forEach { add(it) }
+        layout = BorderLayout(0, 0)
+        add(scrollPane, BorderLayout.CENTER)
+
+        preferredSize = Dimension(150, 120)
+
+        val tilePanel = JPanel()
+        scrollPane.setViewportView(tilePanel)
+        scrollPane.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        scrollPane.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER
+
+        tiles.forEach { tilePanel.add(it) }
     }
 
     fun update(results: List<DartzeeRoundResult>)
