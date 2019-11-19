32009054a4efe29e291625420d3764f317e78c41
alexburlton <alexburlton@hotmail.com>
2019-11-19 13:16:42 +0000

    DartzeeRoundResultRenderer tests

---
+++ Darts/src/burlton/dartzee/test/screen/game/scorer/TestDartzeeRoundResultRenderer.kt
@@ -0,0 +1,54 @@
+package burlton.dartzee.test.screen.game.scorer
+
+import burlton.dartzee.code.screen.dartzee.DartzeeRoundResult
+import burlton.dartzee.code.screen.game.scorer.DartzeeRoundResultRenderer
+import burlton.dartzee.test.helper.AbstractDartsTest
+import io.kotlintest.shouldBe
+import org.junit.Test
+import java.awt.Color
+
+class TestDartzeeRoundResultRenderer: AbstractDartsTest()
+{
+    @Test
+    fun `Should allow nulls`()
+    {
+        DartzeeRoundResultRenderer().allowNulls() shouldBe true
+    }
+
+    @Test
+    fun `Should display a question mark if user input is needed`()
+    {
+        val result = DartzeeRoundResult(-1, false, true)
+
+        val renderer = DartzeeRoundResultRenderer()
+        renderer.getReplacementValue(result) shouldBe "?"
+
+        renderer.setCellColours(result, false)
+        renderer.background shouldBe Color.CYAN
+        renderer.foreground shouldBe Color.BLUE
+    }
+
+    @Test
+    fun `Should render the rule number in green for success`()
+    {
+        val result = DartzeeRoundResult(5, true)
+
+        val renderer = DartzeeRoundResultRenderer()
+        renderer.getReplacementValue(result) shouldBe "#5"
+
+        renderer.setCellColours(result, false)
+        renderer.background shouldBe Color.GREEN
+    }
+
+    @Test
+    fun `Should render the rule number in red for failure`()
+    {
+        val result = DartzeeRoundResult(7, false)
+
+        val renderer = DartzeeRoundResultRenderer()
+        renderer.getReplacementValue(result) shouldBe "#7"
+
+        renderer.setCellColours(result, false)
+        renderer.background shouldBe Color.RED
+    }
+}
