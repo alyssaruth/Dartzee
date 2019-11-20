1b42fe63b3fa9519611c311ebd66fd27c57d965f
alexburlton <alexburlton@hotmail.com>
2019-11-20 07:55:36 +0000

    Simplify logic a bit

--- Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTileHighScore.kt
+++ Darts/src/burlton/dartzee/code/screen/dartzee/DartzeeRuleTileHighScore.kt
@@ -2,7 +2,7 @@ package burlton.dartzee.code.screen.dartzee
 
 import burlton.dartzee.code.dartzee.DartzeeRuleDto
 
-val dto = DartzeeRuleDto(null, null, null, null, false, true)
+val dto = DartzeeRuleDto(null, null, null, null, inOrder = false, allowMisses = true)
 class DartzeeRuleTileHighScore : DartzeeRuleTile(dto, 0)
 {
     init
