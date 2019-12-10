04b0e47d9f8dac9be865785d7e260c7c198022c8
Alex Burlton <alexburlton@hotmail.com>
2019-12-10 13:41:15 +0000

    Flesh out DartzeeRoundResultEntity tests

--- Darts/src/burlton/dartzee/test/db/TestDartzeeRoundResultEntity.kt
+++ Darts/src/burlton/dartzee/test/db/TestDartzeeRoundResultEntity.kt
@@ -1,8 +1,13 @@
 package burlton.dartzee.test.db
 
+import burlton.dartzee.code.dartzee.DartzeeRoundResult
 import burlton.dartzee.code.db.DartzeeRoundResultEntity
+import burlton.dartzee.test.helper.insertParticipant
+import burlton.dartzee.test.helper.insertPlayer
 import io.kotlintest.matchers.collections.shouldContainExactly
+import io.kotlintest.shouldBe
 import org.junit.Test
+import kotlin.test.assertNotNull
 
 class TestDartzeeRoundResultEntity: AbstractEntityTest<DartzeeRoundResultEntity>()
 {
@@ -15,5 +20,42 @@ class TestDartzeeRoundResultEntity: AbstractEntityTest<DartzeeRoundResultEntity>
         factoryDao().addListsOfColumnsForIndexes(indexes)
 
         indexes.first().shouldContainExactly("PlayerId", "ParticipantId", "RoundNumber")
+    }
+
+    @Test
+    fun `Should convert to a DTO correctly`()
+    {
+        val entity = DartzeeRoundResultEntity()
+        entity.roundNumber = 5
+        entity.ruleNumber = 2
+        entity.success = true
+        entity.score = 57
+
+        val dto = entity.toDto()
+
+        dto.ruleNumber shouldBe 2
+        dto.success shouldBe true
+        dto.score shouldBe 57
+    }
+
+    @Test
+    fun `Should save from a DTO correctly`()
+    {
+        val dto = DartzeeRoundResult(5, false, -100)
+
+        val player = insertPlayer()
+        val pt = insertParticipant(playerId = player.rowId)
+
+        DartzeeRoundResultEntity.factoryAndSave(dto, pt, 10)
+
+        val entity = DartzeeRoundResultEntity().retrieveEntities().first()
+        assertNotNull(entity)
+
+        entity.roundNumber shouldBe 10
+        entity.participantId shouldBe pt.rowId
+        entity.playerId shouldBe pt.playerId
+        entity.ruleNumber shouldBe 5
+        entity.success shouldBe false
+        entity.score shouldBe -100
     }
 }
