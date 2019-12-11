6f334d4ebf18626cd89e9faf3fe910acb9e02eb5
alexburlton <alex.burlton@sonocent.com>
2019-12-11 12:55:54 +0000

    More thorough edge point tests

--- Darts/src/burlton/dartzee/test/object/TestDartboardSegment.kt
+++ Darts/src/burlton/dartzee/test/object/TestDartboardSegment.kt
@@ -82,34 +82,92 @@ class TestDartboardSegment: AbstractDartsTest()
         segment.points shouldBe points.toMutableList()
     }
 
+
+    /**
+     * X X X X
+     * X O O X
+     * X O O X
+     * X X X X
+     */
     @Test
-    fun `Should report edge points correctly`()
+    fun `Should report edge points correctly - square`()
     {
         val segment = doubleNineteen.copy()
 
-        val xRange = 0..5
-        val yRange = 0..5
+        val xRange = 0..3
+        val yRange = 0..3
 
         val pts = xRange.map { x -> yRange.map { y -> Point(x, y) } }.flatten()
         pts.forEach { segment.addPoint(it) }
 
         //Corners
         segment.isEdgePoint(Point(0, 0)) shouldBe true
-        segment.isEdgePoint(Point(0, 5)) shouldBe true
-        segment.isEdgePoint(Point(5, 0)) shouldBe true
-        segment.isEdgePoint(Point(5, 5)) shouldBe true
+        segment.isEdgePoint(Point(0, 3)) shouldBe true
+        segment.isEdgePoint(Point(3, 0)) shouldBe true
+        segment.isEdgePoint(Point(3, 3)) shouldBe true
 
         //Random other edges
         segment.isEdgePoint(Point(0, 1)) shouldBe true
+        segment.isEdgePoint(Point(0, 2)) shouldBe true
+        segment.isEdgePoint(Point(3, 1)) shouldBe true
+        segment.isEdgePoint(Point(3, 2)) shouldBe true
         segment.isEdgePoint(Point(1, 0)) shouldBe true
+        segment.isEdgePoint(Point(2, 0)) shouldBe true
+        segment.isEdgePoint(Point(1, 3)) shouldBe true
+        segment.isEdgePoint(Point(2, 3)) shouldBe true
 
         //Inner points
-        segment.isEdgePoint(Point(2, 3)) shouldBe false
+        segment.isEdgePoint(Point(1, 1)) shouldBe false
+        segment.isEdgePoint(Point(1, 2)) shouldBe false
+        segment.isEdgePoint(Point(2, 1)) shouldBe false
         segment.isEdgePoint(Point(2, 2)) shouldBe false
-        segment.isEdgePoint(Point(4, 4)) shouldBe false
 
         //Null case
         segment.isEdgePoint(null) shouldBe false
+    }
+
+    /**         X
+     *        X X
+     *      X O X
+     *    X O O X
+     *  X X X X X
+     *
+     *  ^ This.
+     */
+    @Test
+    fun `Should report edge points correctly - triangle`()
+    {
+        val segment = doubleNineteen.copy()
+
+        val xRange = 0..4
+        val yRange = 0..4
+
+        val pts = xRange.map { x -> yRange.filter{ it <= x }.map { y -> Point(x, y) } }.flatten()
+
+        pts.forEach { segment.addPoint(it) }
+
+        //Bottom edge
+        segment.isEdgePoint(Point(0, 0)) shouldBe true
+        segment.isEdgePoint(Point(1, 0)) shouldBe true
+        segment.isEdgePoint(Point(2, 0)) shouldBe true
+        segment.isEdgePoint(Point(3, 0)) shouldBe true
+        segment.isEdgePoint(Point(4, 0)) shouldBe true
+
+        //Right edge
+        segment.isEdgePoint(Point(4, 1)) shouldBe true
+        segment.isEdgePoint(Point(4, 2)) shouldBe true
+        segment.isEdgePoint(Point(4, 3)) shouldBe true
+        segment.isEdgePoint(Point(4, 4)) shouldBe true
+
+        //Diagonal
+        segment.isEdgePoint(Point(1, 1)) shouldBe true
+        segment.isEdgePoint(Point(2, 2)) shouldBe true
+        segment.isEdgePoint(Point(3, 3)) shouldBe true
+
+        //Inner points
+        segment.isEdgePoint(Point(2, 1)) shouldBe false
+        segment.isEdgePoint(Point(3, 1)) shouldBe false
+        segment.isEdgePoint(Point(3, 2)) shouldBe false
     }
 
 }
