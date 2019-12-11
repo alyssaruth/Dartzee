3ddeb1233bb9a76c5ee93960bcdcfff85b6077ff
Alex Burlton <alexburlton@hotmail.com>
2019-12-11 07:59:46 +0000

    Add TemplateEntity tests

--- Darts/src/burlton/dartzee/test/db/TestDartzeeTemplateEntity.kt
+++ Darts/src/burlton/dartzee/test/db/TestDartzeeTemplateEntity.kt
@@ -1,8 +1,31 @@
 package burlton.dartzee.test.db
 
 import burlton.dartzee.code.db.DartzeeTemplateEntity
+import io.kotlintest.shouldBe
+import org.junit.Test
+import kotlin.test.assertNotNull
 
 class TestDartzeeTemplateEntity: AbstractEntityTest<DartzeeTemplateEntity>()
 {
     override fun factoryDao() = DartzeeTemplateEntity()
+
+    @Test
+    fun `Should use the template name for toString`()
+    {
+        val template = DartzeeTemplateEntity()
+        template.name = "Foo"
+
+        template.toString() shouldBe "Foo"
+    }
+
+    @Test
+    fun `Should factory and save`()
+    {
+        DartzeeTemplateEntity.factoryAndSave("Hello")
+
+        val entityOnDb = DartzeeTemplateEntity().retrieveEntities().first()
+        assertNotNull(entityOnDb)
+
+        entityOnDb.name shouldBe "Hello"
+    }
 }
