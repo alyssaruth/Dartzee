90d959bbd1b447a471f6813ad143695bfaf38ddb
alexburlton <alexburlton@hotmail.com>
2019-12-14 14:09:22 +0000

    Add more creation dialog tests

--- DesktopCore/src/burlton/desktopcore/test/helpers/ComponentHelpers.kt
+++ DesktopCore/src/burlton/desktopcore/test/helpers/ComponentHelpers.kt
@@ -4,6 +4,7 @@ import burlton.core.code.util.Debug
 import burlton.desktopcore.code.bean.ScrollTable
 import io.mockk.mockk
 import java.awt.Component
+import java.awt.event.ActionEvent
 import java.awt.event.MouseEvent
 import java.awt.event.MouseEvent.MOUSE_CLICKED
 import java.awt.event.MouseListener
@@ -16,6 +17,11 @@ val MOUSE_EVENT_SINGLE_CLICK = MouseEvent(JButton(), MOUSE_CLICKED, System.curre
 fun makeMouseEvent(clickCount: Int = 1, x: Int = 0, y: Int = 0, component: Component = JButton()): MouseEvent
 {
     return MouseEvent(component, MOUSE_CLICKED, System.currentTimeMillis(), -1, x, y, clickCount, false)
+}
+
+fun makeActionEvent(component: Component): ActionEvent
+{
+    return ActionEvent(component, 0, null)
 }
 
 /**
