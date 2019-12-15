de892e85457332e81d1329150de9edbeabbeca1b
alexburlton <alexburlton@hotmail.com>
2019-12-15 15:15:50 +0000

    Formatting

--- DesktopCore/src/burlton/desktopcore/code/bean/ComboBoxItem.kt
+++ DesktopCore/src/burlton/desktopcore/code/bean/ComboBoxItem.kt
@@ -1,6 +1,6 @@
 package burlton.desktopcore.code.bean
 
-data class ComboBoxItem<E>(val hiddenData: E, val visibleData: Any, var isEnabled: Boolean  =true)
+data class ComboBoxItem<E>(val hiddenData: E, val visibleData: Any, var isEnabled: Boolean = true)
 {
     override fun toString() = if (isEnabled) "$visibleData" else "<html><font color=\"gray\">$visibleData</font></html>"
 }
