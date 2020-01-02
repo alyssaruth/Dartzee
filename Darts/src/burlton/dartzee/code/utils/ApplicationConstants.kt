7ae0982bd0aabcdeaf7b3964429fa50a0832ecdb
alexburlton <alexburlton@hotmail.com>
2020-01-02 18:18:40 +0000

    Bump version, add to change log

--- Darts/src/burlton/dartzee/code/utils/ApplicationConstants.kt
+++ Darts/src/burlton/dartzee/code/utils/ApplicationConstants.kt
@@ -1,5 +1,5 @@
 package burlton.dartzee.code.utils
 
-const val DARTS_VERSION_NUMBER = "v4.0.0"
+const val DARTS_VERSION_NUMBER = "v4.0.1"
 const val DARTZEE_REPOSITORY_URL = "https://api.github.com/repos/alexburlton/Dartzee"
 const val MAX_SQL_DURATION = 5000
