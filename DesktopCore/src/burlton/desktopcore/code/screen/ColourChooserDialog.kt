0da9c3ee93d9461daee8cd8e4fb48b090d8a7e9f
Alex Burlton <alexburlton@hotmail.com>
2020-01-03 13:40:10 +0000

    Tests for ColourChooserDialog

--- DesktopCore/src/burlton/desktopcore/code/screen/ColourChooserDialog.kt
+++ DesktopCore/src/burlton/desktopcore/code/screen/ColourChooserDialog.kt
@@ -1,48 +1,38 @@
 package burlton.desktopcore.code.screen
 
 import burlton.desktopcore.code.bean.IColourSelector
-import java.awt.BorderLayout
 import java.awt.Color
-import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
-import javax.swing.JButton
 import javax.swing.JColorChooser
-import javax.swing.JDialog
-import javax.swing.JPanel
 import javax.swing.colorchooser.DefaultColorSelectionModel
 
-class ColourChooserDialog : JDialog(), ActionListener, IColourSelector
+class ColourChooserDialog : SimpleDialog(), ActionListener, IColourSelector
 {
-    private var initialColour: Color = Color.BLACK
+    var initialColour: Color = Color.BLACK
     var selectedColour: Color = Color.BLACK
 
-    private val colourChooser = JColorChooser(DefaultColorSelectionModel())
-    private val btnOk = JButton("Ok")
-    private val btnCancel = JButton("Cancel")
+    val colourChooser = JColorChooser(DefaultColorSelectionModel())
 
     init
     {
         title = "Choose Colour"
         setSize(660, 450)
         contentPane.add(colourChooser)
-        val panel = JPanel()
-        contentPane.add(panel, BorderLayout.SOUTH)
         isModal = true
 
-        panel.add(btnOk)
-        panel.add(btnCancel)
         btnOk.addActionListener(this)
         btnCancel.addActionListener(this)
     }
 
-    override fun actionPerformed(e: ActionEvent)
+    override fun okPressed()
     {
-        when (e.source)
-        {
-            btnOk -> selectedColour = colourChooser.color
-            btnCancel -> selectedColour = initialColour
-        }
+        selectedColour = colourChooser.color
+        dispose()
+    }
 
+    override fun cancelPressed()
+    {
+        selectedColour = initialColour
         dispose()
     }
 
