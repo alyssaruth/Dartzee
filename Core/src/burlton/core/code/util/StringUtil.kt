6a07db828c2166fa0b63690f27a5bb25507d8614
Alex Burlton <alexburlton@hotmail.com>
2020-01-08 08:55:50 +0000

    Inline

--- Core/src/burlton/core/code/util/StringUtil.kt
+++ Core/src/burlton/core/code/util/StringUtil.kt
@@ -7,8 +7,7 @@ object StringUtil
         if (position < 0) return ""
 
         val suffixes = listOf("th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th")
-        val remainder = position % 100
-        return when (remainder)
+        return when (position % 100)
         {
             11, 12, 13 -> "${position}th"
             else -> "${position}${suffixes[position % 10]}"
