07f7d3871e0a40f746a31a7f15e7904de38e84c3
Alex Burlton <alexburlton@hotmail.com>
2020-01-08 08:44:36 +0000

    Tidy up the KeyAction stuff some more

--- DesktopCore/src/burlton/desktopcore/code/bean/SwingExtensions.kt
+++ DesktopCore/src/burlton/desktopcore/code/bean/SwingExtensions.kt
@@ -5,9 +5,7 @@ import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.awt.event.FocusEvent
 import java.awt.event.FocusListener
-import javax.swing.AbstractAction
-import javax.swing.JComboBox
-import javax.swing.JTextField
+import javax.swing.*
 import javax.swing.text.JTextComponent
 
 
@@ -43,15 +41,15 @@ fun JTextField.addUpdateListener(actionListener: ActionListener)
     })
 }
 
-fun ScrollTable.addKeyAction(key: Int, fn: () -> Unit)
-{
+fun JTable.addKeyAction(key: Int, fn: () -> Unit) {
     val action = object : AbstractAction() {
         override fun actionPerformed(e: ActionEvent?) {
             fn()
         }
     }
 
-    addKeyAction(key, "ACTION_$key", action)
+    inputMap.put(KeyStroke.getKeyStroke(key, JComponent.WHEN_FOCUSED),"ACTION_$key")
+    actionMap.put("ACTION_$key", action)
 }
 
 fun JTextComponent.addGhostText(text: String)
