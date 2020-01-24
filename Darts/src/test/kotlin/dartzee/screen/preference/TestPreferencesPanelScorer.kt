cb7537cca952bdcd93293303ccef345999c5008b
Alex <alex.burlton@sonocent.com>
2020-01-24 12:30:35 +0000

    Axe the dart notation preference - unnecessary complication (and also fixes a test flake!)

--- Darts/src/test/kotlin/dartzee/screen/preference/TestPreferencesPanelScorer.kt
+++ Darts/src/test/kotlin/dartzee/screen/preference/TestPreferencesPanelScorer.kt
@@ -1,7 +1,9 @@
 package dartzee.screen.preference
 
-import dartzee.screen.preference.PreferencesPanelScorer
-import dartzee.utils.*
+import dartzee.utils.PREFERENCES_DOUBLE_BG_BRIGHTNESS
+import dartzee.utils.PREFERENCES_DOUBLE_FG_BRIGHTNESS
+import dartzee.utils.PREFERENCES_DOUBLE_HUE_FACTOR
+import dartzee.utils.PreferenceUtil
 import io.kotlintest.matchers.collections.shouldBeEmpty
 import io.kotlintest.matchers.collections.shouldContainExactly
 import io.kotlintest.shouldBe
@@ -37,16 +39,13 @@ class TestPreferencesPanelScorer: AbstractPreferencePanelTest<PreferencesPanelSc
     {
         return mutableListOf(PREFERENCES_DOUBLE_HUE_FACTOR,
                 PREFERENCES_DOUBLE_BG_BRIGHTNESS,
-                PREFERENCES_DOUBLE_FG_BRIGHTNESS,
-                PREFERENCES_BOOLEAN_DISPLAY_DART_TOTAL_SCORE)
+                PREFERENCES_DOUBLE_FG_BRIGHTNESS)
     }
 
     override fun factory() = PreferencesPanelScorer()
 
     override fun checkUiFieldValuesAreDefaults(panel: PreferencesPanelScorer)
     {
-        panel.rdbtn40.isSelected shouldBe true
-        panel.rdbtnD20.isSelected shouldBe false
         panel.spinnerHueFactor.value shouldBe 0.8
         panel.spinnerFgBrightness.value shouldBe 0.5
         panel.spinnerBgBrightness.value shouldBe 1.0
@@ -54,8 +53,6 @@ class TestPreferencesPanelScorer: AbstractPreferencePanelTest<PreferencesPanelSc
 
     override fun setUiFieldValuesToNonDefaults(panel: PreferencesPanelScorer)
     {
-        panel.rdbtn40.isSelected = false
-        panel.rdbtnD20.isSelected = true
         panel.spinnerHueFactor.value = 0.5
         panel.spinnerFgBrightness.value = 0.9
         panel.spinnerBgBrightness.value = 0.6
@@ -63,8 +60,6 @@ class TestPreferencesPanelScorer: AbstractPreferencePanelTest<PreferencesPanelSc
 
     override fun checkUiFieldValuesAreNonDefaults(panel: PreferencesPanelScorer)
     {
-        panel.rdbtn40.isSelected shouldBe false
-        panel.rdbtnD20.isSelected shouldBe true
         panel.spinnerHueFactor.value shouldBe 0.5
         panel.spinnerFgBrightness.value shouldBe 0.9
         panel.spinnerBgBrightness.value shouldBe 0.6
@@ -72,7 +67,6 @@ class TestPreferencesPanelScorer: AbstractPreferencePanelTest<PreferencesPanelSc
 
     override fun checkPreferencesAreSetToNonDefaults()
     {
-        PreferenceUtil.getBooleanValue(PREFERENCES_BOOLEAN_DISPLAY_DART_TOTAL_SCORE) shouldBe false
         PreferenceUtil.getDoubleValue(PREFERENCES_DOUBLE_HUE_FACTOR) shouldBe 0.5
         PreferenceUtil.getDoubleValue(PREFERENCES_DOUBLE_FG_BRIGHTNESS) shouldBe 0.9
         PreferenceUtil.getDoubleValue(PREFERENCES_DOUBLE_BG_BRIGHTNESS) shouldBe 0.6
