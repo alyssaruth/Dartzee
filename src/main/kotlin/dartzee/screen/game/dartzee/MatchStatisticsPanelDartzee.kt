8a86879e1b887a3091306023abafbee6c3e6227e
alexburlton <alexburlton@hotmail.com>
2020-03-21 13:42:46 +0000

    Plug in the stuff needed for the Dartzee match summary

--- src/main/kotlin/dartzee/screen/game/dartzee/MatchStatisticsPanelDartzee.kt
+++ src/main/kotlin/dartzee/screen/game/dartzee/MatchStatisticsPanelDartzee.kt
@@ -1,3 +1,22 @@
 package dartzee.screen.game.dartzee
 
+import dartzee.core.util.maxOrZero
+
 class MatchStatisticsPanelDartzee(gameParams: String): GameStatisticsPanelDartzee(gameParams)
+{
+    override fun addRowsToTable()
+    {
+        super.addRowsToTable()
+
+        addRow(getBestGameRow { it.maxOrZero() })
+        addRow(getAverageGameRow())
+    }
+
+    override fun getRankedRowsHighestWins(): MutableList<String>
+    {
+        val rows = super.getRankedRowsHighestWins()
+        rows.add("Best Game")
+        rows.add("Avg Game")
+        return rows
+    }
+}
