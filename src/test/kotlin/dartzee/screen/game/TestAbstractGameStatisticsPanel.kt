a38c61462e0d7ed769f1e441b9933a2d59194e22
Alex Burlton <alexburlton@hotmail.com>
2020-03-26 08:44:53 +0000

    histogram rendering test

--- src/test/kotlin/dartzee/screen/game/TestAbstractGameStatisticsPanel.kt
+++ src/test/kotlin/dartzee/screen/game/TestAbstractGameStatisticsPanel.kt
@@ -115,21 +115,21 @@ class TestAbstractGameStatisticsPanelRenderers: AbstractTest()
     }
 
     @Test
-    fun `Should additionally set top borders if row is end of section`()
+    fun `Should additionally set top borders if row is start of section`()
     {
         val state1 = makeDefaultPlayerState(insertPlayer(name = "Alice"), dartsThrown = listOf(makeDart(), makeDart(), makeDart()))
         val state2 = makeDefaultPlayerState(insertPlayer(name = "Bob"), dartsThrown = listOf(makeDart()))
         val state3 = makeDefaultPlayerState(insertPlayer(name = "Clive"), dartsThrown = listOf(makeDart(), makeDart(), makeDart()))
         val state4 = makeDefaultPlayerState(insertPlayer(name = "Derek"), dartsThrown = listOf(makeDart(), makeDart(), makeDart(), makeDart()))
 
-        val panel = FakeGameStatisticsPanel(sectionStarts = listOf("Darts Thrown"))
+        val panel = FakeGameStatisticsPanel(sectionStarts = listOf("20s"))
         panel.showStats(listOf(state1, state2, state3, state4))
 
-        panel.getCellComponent(0, 0).shouldHaveBorderThickness(2, 1, 2, 2)
-        panel.getCellComponent(0, 1).shouldHaveBorderThickness(1, 1, 2, 2)
-        panel.getCellComponent(0, 2).shouldHaveBorderThickness(1, 1, 2, 2)
-        panel.getCellComponent(0, 3).shouldHaveBorderThickness(1, 1, 2, 2)
-        panel.getCellComponent(0, 4).shouldHaveBorderThickness(1, 2, 2, 2)
+        panel.getCellComponent(1, 0).shouldHaveBorderThickness(2, 1, 2, 2)
+        panel.getCellComponent(1, 1).shouldHaveBorderThickness(1, 1, 2, 2)
+        panel.getCellComponent(1, 2).shouldHaveBorderThickness(1, 1, 2, 2)
+        panel.getCellComponent(1, 3).shouldHaveBorderThickness(1, 1, 2, 2)
+        panel.getCellComponent(1, 4).shouldHaveBorderThickness(1, 2, 2, 2)
     }
 
     @Test
@@ -142,6 +142,27 @@ class TestAbstractGameStatisticsPanelRenderers: AbstractTest()
 
         panel.getCellComponent(0, 0).shouldHaveColours(Pair(Color.WHITE, null))
         panel.getCellComponent(0, 0).font.isBold shouldBe true
+    }
+
+    @Test
+    fun `Should colour histogram rows correctly`()
+    {
+        val state1 = makeDefaultPlayerState(insertPlayer(name = "Alice"),
+            dartsThrown = listOf(makeDart(20), makeDart(20), makeDart(5), makeDart(5), makeDart(5)))
+
+        val state2 = makeDefaultPlayerState(insertPlayer(name = "Bob"),
+            dartsThrown = listOf(makeDart(20), makeDart(5), makeDart(2), makeDart(1)))
+
+        val panel = FakeGameStatisticsPanel(histograms = listOf("20s", "Others"))
+        panel.showStats(listOf(state1, state2))
+
+        //Alice: 40% - 60%
+        panel.getCellComponent(1, 1).shouldHaveColours(Pair(Color.getHSBColor(0.5.toFloat(), 0.4f, 1f) , null))
+        panel.getCellComponent(2, 1).shouldHaveColours(Pair(Color.getHSBColor(0.5.toFloat(), 0.6f, 1f) , null))
+
+        //Bob: 25% - 75%
+        panel.getCellComponent(1, 2).shouldHaveColours(Pair(Color.getHSBColor(0.5.toFloat(), 0.25f, 1f) , null))
+        panel.getCellComponent(2, 2).shouldHaveColours(Pair(Color.getHSBColor(0.5.toFloat(), 0.75f, 1f) , null))
     }
 }
 
@@ -173,11 +194,23 @@ private class FakeGameStatisticsPanel(private val highestWins: List<String> = em
 
     override fun addRowsToTable() {
         addRow(getDartsThrownRow())
+        addRow(getTwentiesRow())
+        addRow(getOtherDartsRow())
     }
 
     private fun getDartsThrownRow() = prepareRow("Darts Thrown") { playerName ->
         val darts = hmPlayerToDarts[playerName] ?: emptyList()
         darts.map { it.size }.sum()
+    }
+
+    private fun getTwentiesRow() = prepareRow("20s") { playerName ->
+        val darts = hmPlayerToDarts[playerName]?.flatten() ?: emptyList()
+        darts.filter { it.score == 20 }.size
+    }
+
+    private fun getOtherDartsRow() = prepareRow("Others") { playerName ->
+        val darts = hmPlayerToDarts[playerName]?.flatten() ?: emptyList()
+        darts.filter { it.score != 20 }.size
     }
 
     fun getCellComponent(row: Int, column: Int): JComponent
