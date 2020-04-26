bb3d67b1439721c59c98f36968b2df3c72860ccf
Alex Burlton <alexburlton@hotmail.com>
2020-04-26 14:47:51 +0100

    Tighten up error handling

--- src/main/kotlin/dartzee/logging/LogDestinationElasticsearch.kt
+++ src/main/kotlin/dartzee/logging/LogDestinationElasticsearch.kt
@@ -5,7 +5,9 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.http.AWSRequestSigningApacheInterceptor
 import com.amazonaws.services.elasticsearch.AWSElasticsearch
+import dartzee.utils.InjectedThings.logger
 import org.apache.http.HttpHost
+import org.apache.http.HttpStatus
 import org.apache.http.entity.ContentType
 import org.apache.http.nio.entity.NStringEntity
 import org.elasticsearch.client.Request
@@ -21,7 +23,7 @@ private const val ELASTICSEARCH_URL = "https://search-dartzee-nfqeufkxsx6cu7sybh
 
 class LogDestinationElasticsearch: ILogDestination
 {
-    val pendingLogs = ConcurrentHashMap.newKeySet<LogRecord>()
+    private val pendingLogs = ConcurrentHashMap.newKeySet<LogRecord>()
 
     private val scheduler = Executors.newScheduledThreadPool(1)
     private val client = createClient()
@@ -34,23 +36,32 @@ class LogDestinationElasticsearch: ILogDestination
 
     override fun log(record: LogRecord)
     {
-        pendingLogs.add(record)
+        client?.let { pendingLogs.add(record) }
     }
 
     override fun contextUpdated(context: Map<String, Any?>){}
 
 
-    private fun createClient(): RestClient
+    private fun createClient(): RestClient?
     {
-        val signer = AWS4Signer().also {
-            it.serviceName = AWSElasticsearch.ENDPOINT_PREFIX
-            it.regionName = "eu-west-2"
-        }
+        try
+        {
+            val signer = AWS4Signer().also {
+                it.serviceName = AWSElasticsearch.ENDPOINT_PREFIX
+                it.regionName = "eu-west-2"
+            }
 
-        val credentials = readCredentials()
-        val provider = AWSStaticCredentialsProvider(credentials)
-        val interceptor = AWSRequestSigningApacheInterceptor(signer.serviceName, signer, provider)
-        return RestClient.builder(HttpHost.create(ELASTICSEARCH_URL)).setHttpClientConfigCallback { it.addInterceptorLast(interceptor) }.build()
+            val credentials = readCredentials()
+            val provider = AWSStaticCredentialsProvider(credentials)
+            val interceptor = AWSRequestSigningApacheInterceptor(signer.serviceName, signer, provider)
+            return RestClient.builder(HttpHost.create(ELASTICSEARCH_URL))
+                .setHttpClientConfigCallback { it.addInterceptorLast(interceptor) }.build()
+        }
+        catch (t: Throwable)
+        {
+            logger.error(CODE_ELASTICSEARCH_ERROR, "Failed to set up RestClient - won't post logs to ES", t)
+            return null
+        }
     }
     private fun readCredentials(): BasicAWSCredentials
     {
@@ -60,14 +71,37 @@ class LogDestinationElasticsearch: ILogDestination
         return BasicAWSCredentials(lines[0], lines[1])
     }
 
-    fun postPendingLogs()
+    private fun postPendingLogs()
     {
         val logsForThisRun = pendingLogs.toList()
-        logsForThisRun.forEach {
+        logsForThisRun.forEach(::postLogToElasticsearch)
+    }
+    private fun postLogToElasticsearch(log: LogRecord)
+    {
+        client ?: return
+
+        val logJson = log.toJsonString()
+
+        try
+        {
             val request = Request("PUT", "$INDEX_PATH/${UUID.randomUUID()}")
-            request.entity = NStringEntity(it.toJsonString(), ContentType.APPLICATION_JSON)
+            request.entity = NStringEntity(logJson, ContentType.APPLICATION_JSON)
+
             val response = client.performRequest(request)
-            println(response)
+            val status = response.statusLine.statusCode
+            if (status == HttpStatus.SC_CREATED)
+            {
+                pendingLogs.remove(log)
+            }
+            else
+            {
+                logger.error(CODE_ELASTICSEARCH_ERROR, "Received status code $status trying to post to ES", Throwable(), KEY_RESPONSE_BODY to response)
+            }
         }
+        catch(t: Throwable)
+        {
+            logger.error(CODE_ELASTICSEARCH_ERROR, "Failed to post log to ES", t)
+        }
+
     }
 }
