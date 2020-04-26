9a2be1c54788f186c8f6c8c910c568964603336c
Alex Burlton <alexburlton@hotmail.com>
2020-04-26 13:52:10 +0100

    WIP

--- src/main/kotlin/dartzee/logging/LogDestinationElasticsearch.kt
+++ src/main/kotlin/dartzee/logging/LogDestinationElasticsearch.kt
@@ -1,10 +1,26 @@
 package dartzee.logging
 
+import com.amazonaws.auth.AWS4Signer
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.services.elasticsearch.AWSElasticsearch
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+private const val INDEX_PATH = "/dartzee/_doc/_create"
 
 class LogDestinationElasticsearch: ILogDestination
 {
     val pendingLogs = ConcurrentHashMap.newKeySet<LogRecord>()
+
+    private val scheduler = Executors.newScheduledThreadPool(1)
+    private val signer = factorySigner()
+
+    init
+    {
+        val runnable = Runnable { postPendingLogs() }
+        scheduler.scheduleAtFixedRate(runnable, 30, 30, TimeUnit.SECONDS)
+    }
 
     override fun log(record: LogRecord)
     {
@@ -12,4 +28,20 @@ class LogDestinationElasticsearch: ILogDestination
     }
 
     override fun contextUpdated(context: Map<String, Any?>){}
+
+
+    private fun createClient() {
+        val signer = AWS4Signer().also {
+            it.serviceName = AWSElasticsearch.ENDPOINT_PREFIX
+            it.regionName = "eu-west-2"
+        }
+
+        val interceptor = AWSRequestSigningApacheInterceptor(signer.serviceName, signer, DefaultAWSCredentialsProviderChain())
+
+    }
+    fun postPendingLogs()
+    {
+        val logsForThisRun = pendingLogs.size
+
+    }
 }
