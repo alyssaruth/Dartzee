b91e02d8580c443532fcfea300a2800d3266d842
Alex Burlton <alexburlton@hotmail.com>
2020-04-29 20:33:58 +0100

    Tests

--- src/main/kotlin/dartzee/bean/SliderAiSpeed.kt
+++ src/main/kotlin/dartzee/bean/SliderAiSpeed.kt
@@ -9,6 +9,9 @@ import javax.swing.JSlider
 import javax.swing.plaf.synth.SynthContext
 import javax.swing.plaf.synth.SynthSliderUI
 
+const val AI_SPEED_MINIMUM = 0 //0s
+const val AI_SPEED_MAXIMUM = 2000 //2s
+
 class SliderAiSpeed(custom: Boolean) : JSlider()
 {
     init
@@ -21,26 +24,20 @@ class SliderAiSpeed(custom: Boolean) : JSlider()
 
         if (custom && !DartsClient.isAppleOs())
         {
-            setUI(CustomUI(this))
+            setUI(CustomSliderUI(this))
         }
     }
+}
 
-    private class CustomUI(arg0: JSlider) : SynthSliderUI(arg0)
+class CustomSliderUI(arg0: JSlider) : SynthSliderUI(arg0)
+{
+    override fun getThumbSize() = Dimension(30, 30)
+
+    override fun paintTrack(arg0: SynthContext, arg1: Graphics,
+                            arg2: Rectangle)
     {
-        override fun getThumbSize() = Dimension(30, 30)
+        arg2.setSize(30, 200)
 
-        override fun paintTrack(arg0: SynthContext, arg1: Graphics,
-                                arg2: Rectangle)
-        {
-            arg2.setSize(30, 200)
-
-            super.paintTrack(arg0, arg1, arg2)
-        }
-    }
-
-    companion object
-    {
-        private const val AI_SPEED_MINIMUM = 0 //0s
-        private const val AI_SPEED_MAXIMUM = 2000 //2s
+        super.paintTrack(arg0, arg1, arg2)
     }
 }
