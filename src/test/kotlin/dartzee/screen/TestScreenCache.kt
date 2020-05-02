1cd2728c6cdfb1f028fdbcb7f5eb782cdaa9bb33
alexburlton <alexburlton@hotmail.com>
2020-05-02 08:56:32 +0100

    Finish ScreenCache tests

--- src/test/kotlin/dartzee/screen/TestScreenCache.kt
+++ src/test/kotlin/dartzee/screen/TestScreenCache.kt
@@ -2,9 +2,12 @@ package dartzee.screen
 
 import dartzee.core.helper.verifyNotCalled
 import dartzee.helper.AbstractTest
+import dartzee.screen.game.DartsGameScreen
+import io.kotlintest.matchers.collections.shouldContainExactly
 import io.kotlintest.matchers.types.shouldBeSameInstanceAs
 import io.kotlintest.matchers.types.shouldNotBeNull
 import io.kotlintest.matchers.types.shouldNotBeSameInstanceAs
+import io.kotlintest.shouldBe
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
@@ -24,11 +27,13 @@ class TestScreenCache: AbstractTest()
     @Test
     fun `Should clear the cache`()
     {
+        ScreenCache.addDartsGameScreen("Game 1", mockk())
         val scrn = ScreenCache.get<MenuScreen>()
         ScreenCache.emptyCache()
         val other = ScreenCache.get<MenuScreen>()
 
         scrn shouldNotBeSameInstanceAs other
+        ScreenCache.getDartsGameScreen("Game 1") shouldBe null
     }
 
     @Test
@@ -65,6 +70,42 @@ class TestScreenCache: AbstractTest()
     @Test
     fun `Should return the unique count of darts game screens`()
     {
+        val screenOne = mockk<DartsGameScreen>(relaxed = true)
+        val screenTwo = mockk<DartsGameScreen>(relaxed = true)
 
+        ScreenCache.addDartsGameScreen("Game 1", screenOne)
+        ScreenCache.addDartsGameScreen("Game 2", screenOne)
+        ScreenCache.addDartsGameScreen("Game 3", screenTwo)
+
+        val screens = ScreenCache.getDartsGameScreens()
+        screens.size shouldBe 2
+        screens.shouldContainExactly(screenOne, screenTwo)
+    }
+
+    @Test
+    fun `Should be able to get the screen for a particular GameId`()
+    {
+        ScreenCache.getDartsGameScreen("Game 1") shouldBe null
+
+        val screenOne = mockk<DartsGameScreen>(relaxed = true)
+        ScreenCache.addDartsGameScreen("Game 1", screenOne)
+        ScreenCache.getDartsGameScreen("Game 1") shouldBe screenOne
+    }
+
+    @Test
+    fun `Should be able to remove a screen`()
+    {
+        val screenOne = mockk<DartsGameScreen>(relaxed = true)
+        val screenTwo = mockk<DartsGameScreen>(relaxed = true)
+
+        ScreenCache.addDartsGameScreen("Game 1", screenOne)
+        ScreenCache.addDartsGameScreen("Game 2", screenOne)
+        ScreenCache.addDartsGameScreen("Game 3", screenTwo)
+
+        ScreenCache.removeDartsGameScreen(screenOne)
+
+        ScreenCache.getDartsGameScreen("Game 1") shouldBe null
+        ScreenCache.getDartsGameScreen("Game 2") shouldBe null
+        ScreenCache.getDartsGameScreen("Game 3") shouldBe screenTwo
     }
 }
