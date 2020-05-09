bc55cc8b78f03a8dd46bf0d0f2210bcc826d8c2d
alexburlton <alexburlton@hotmail.com>
2020-05-09 10:48:38 +0100

    formatting

--- src/test/kotlin/dartzee/helper/InMemoryDatabaseHelper.kt
+++ src/test/kotlin/dartzee/helper/InMemoryDatabaseHelper.kt
@@ -60,9 +60,9 @@ fun insertDartsMatch(uuid: String = randomGuid(),
 }
 
 fun insertGameForPlayer(player: PlayerEntity,
-                                gameType: GameType = GameType.X01,
-                                finalScore: Int = -1,
-                                dtFinished: Timestamp = DateStatics.END_OF_TIME)
+                        gameType: GameType = GameType.X01,
+                        finalScore: Int = -1,
+                        dtFinished: Timestamp = DateStatics.END_OF_TIME)
 {
     val game = insertGame(gameType = gameType)
     insertParticipant(playerId = player.rowId, gameId = game.rowId, finalScore = finalScore, dtFinished = dtFinished)
