d0ee4f82a3fcddc63884ca8b900544208a872613
alexburlton <alexburlton@hotmail.com>
2020-05-23 13:22:01 +0100

    More tests for points risked conversion

--- src/test/kotlin/dartzee/achievements/golf/TestAchievementGolfPointsRisked.kt
+++ src/test/kotlin/dartzee/achievements/golf/TestAchievementGolfPointsRisked.kt
@@ -17,6 +17,35 @@ class TestAchievementGolfPointsRisked: AbstractAchievementTest<AchievementGolfPo
     override fun factoryAchievement() = AchievementGolfPointsRisked()
     override fun setUpAchievementRowForPlayerAndGame(p: PlayerEntity, g: GameEntity) = insertRiskedDart(p, g)
 
+    @Test
+    fun `Should ignore rounds where no points were risked`()
+    {
+        val p = insertPlayer()
+        val g = insertRelevantGame()
+
+        val pt = insertParticipant(playerId = p.rowId, gameId = g.rowId)
+        insertDart(pt, roundNumber = 1, ordinal = 1, score = 1, multiplier = 1, segmentType = SegmentType.DOUBLE)
+
+        factoryAchievement().populateForConversion("")
+
+        AchievementEntity.retrieveAchievement(ACHIEVEMENT_REF_GOLF_POINTS_RISKED, p.rowId) shouldBe null
+    }
+
+    @Test
+    fun `Should combine multiple rounds correctly`()
+    {
+        val p = insertPlayer()
+        val g = insertRelevantGame()
+
+        insertRiskedDart(p, g, SegmentType.INNER_SINGLE, 1)
+        insertRiskedDart(p, g, SegmentType.OUTER_SINGLE, 2)
+
+        factoryAchievement().populateForConversion("")
+
+        val a = AchievementEntity.retrieveAchievement(ACHIEVEMENT_REF_GOLF_POINTS_RISKED, p.rowId)!!
+        a.achievementCounter shouldBe 3
+    }
+
 
     @Test
     fun `Should map segment types correctly`()
@@ -40,12 +69,12 @@ class TestAchievementGolfPointsRisked: AbstractAchievementTest<AchievementGolfPo
         a.achievementCounter shouldBe expectedScore
     }
 
-    fun insertRiskedDart(p: PlayerEntity, g: GameEntity, segmentType: SegmentType = SegmentType.OUTER_SINGLE)
+    fun insertRiskedDart(p: PlayerEntity, g: GameEntity, segmentType: SegmentType = SegmentType.OUTER_SINGLE, roundNumber: Int = 1)
     {
         val pt = insertParticipant(playerId = p.rowId, gameId = g.rowId)
 
-        insertDart(pt, roundNumber = 1, ordinal = 1, score = 1, multiplier = 1, segmentType = segmentType)
-        insertDart(pt, roundNumber = 1, ordinal = 2, score = 1, multiplier = 2, segmentType = SegmentType.DOUBLE)
+        insertDart(pt, roundNumber = roundNumber, ordinal = 1, score = roundNumber, multiplier = 1, segmentType = segmentType)
+        insertDart(pt, roundNumber = roundNumber, ordinal = 2, score = roundNumber, multiplier = 2, segmentType = SegmentType.DOUBLE)
 
     }
 }
