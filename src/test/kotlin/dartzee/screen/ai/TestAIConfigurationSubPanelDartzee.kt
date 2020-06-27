323f69e3f2a8d7fc4af94f63f9b468ff2cafaf8f
alexburlton <alexburlton@hotmail.com>
2020-06-27 15:02:01 +0100

    sub panel tests

---
+++ src/test/kotlin/dartzee/screen/ai/TestAIConfigurationSubPanelDartzee.kt
@@ -0,0 +1,45 @@
+package dartzee.screen.ai
+
+import com.github.alexburlton.swingtest.clickChild
+import com.github.alexburlton.swingtest.getChild
+import dartzee.ai.DartsModelNormalDistribution
+import dartzee.ai.DartzeePlayStyle
+import dartzee.helper.AbstractTest
+import io.kotlintest.shouldBe
+import org.junit.Test
+import javax.swing.JRadioButton
+
+class TestAIConfigurationSubPanelDartzee: AbstractTest()
+{
+    @Test
+    fun `Should initialise from model correctly`()
+    {
+        val model = DartsModelNormalDistribution()
+        model.dartzeePlayStyle = DartzeePlayStyle.AGGRESSIVE
+
+        val panel = AIConfigurationSubPanelDartzee()
+        panel.initialiseFromModel(model)
+        panel.getChild<JRadioButton>("Cautious").isSelected shouldBe false
+        panel.getChild<JRadioButton>("Aggressive").isSelected shouldBe true
+
+        model.dartzeePlayStyle = DartzeePlayStyle.CAUTIOUS
+        panel.initialiseFromModel(model)
+        panel.getChild<JRadioButton>("Cautious").isSelected shouldBe true
+        panel.getChild<JRadioButton>("Aggressive").isSelected shouldBe false
+    }
+
+    @Test
+    fun `Should populate model correctly`()
+    {
+        val model = DartsModelNormalDistribution()
+        val panel = AIConfigurationSubPanelDartzee()
+
+        panel.clickChild<JRadioButton>("Cautious")
+        panel.populateModel(model)
+        model.dartzeePlayStyle shouldBe DartzeePlayStyle.CAUTIOUS
+
+        panel.clickChild<JRadioButton>("Aggressive")
+        panel.populateModel(model)
+        model.dartzeePlayStyle shouldBe DartzeePlayStyle.AGGRESSIVE
+    }
+}
