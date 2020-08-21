4b1078f2419ee7c7f008f2f3833707e282839f79
alexburlton <alexburlton@hotmail.com>
2020-08-21 16:06:46 +0100

    Add proper RTC config class for serializing as GameParams

---
+++ src/main/kotlin/dartzee/game/RoundTheClockConfig.kt
@@ -0,0 +1,21 @@
+package dartzee.game
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import dartzee.core.util.jsonMapper
+
+enum class ClockType
+{
+    STANDARD,
+    DOUBLE,
+    TREBLE
+}
+
+data class RoundTheClockConfig(val clockType: ClockType, val inOrder: Boolean)
+{
+    fun toJson(): String = jsonMapper().writeValueAsString(this)
+
+    companion object
+    {
+        fun fromJson(json: String) = jsonMapper().readValue<RoundTheClockConfig>(json)
+    }
+}
