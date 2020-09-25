96035126489f154351896b1bf270a59531cf8a78
alexburlton <alexburlton@hotmail.com>
2020-09-25 21:37:00 +0100

    start on X01 tests - rm stupid abstract test class

--- src/test/kotlin/dartzee/screen/game/scorer/TestDartsScorerX01.kt
+++ src/test/kotlin/dartzee/screen/game/scorer/TestDartsScorerX01.kt
@@ -1,24 +1,23 @@
 package dartzee.screen.game.scorer
 
-import dartzee.`object`.Dart
+import dartzee.helper.AbstractTest
 import io.kotlintest.shouldBe
 import io.mockk.mockk
 import org.junit.Test
 
-class TestDartsScorerX01: AbstractScorerTest<DartsScorerX01>()
+class TestDartsScorerX01: AbstractTest()
 {
-    override fun factoryScorerImpl() = DartsScorerX01(mockk(relaxed = true), "501")
-    override fun addRound(scorer: DartsScorerX01, roundNumber: Int)
-    {
-        scorer.addDart(Dart(20, 1))
-        scorer.addDart(Dart(20, 2))
-        scorer.addDart(Dart(20, 1))
-    }
-
     @Test
     fun `should have 4 columns`()
     {
         val scorer = factoryScorer()
         scorer.getNumberOfColumns() shouldBe 4
+    }
+
+    private fun factoryScorer(): DartsScorerX01
+    {
+        val scorer = DartsScorerX01(mockk(relaxed = true), "501")
+        scorer.init(null)
+        return scorer
     }
 }
