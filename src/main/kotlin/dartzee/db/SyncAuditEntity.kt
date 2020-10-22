de23b3e8fd0dc162c86a4eaf047d185540ae16c3
Alex Burlton <alexburlton@hotmail.com>
2020-10-22 13:17:47 +0100

    further SyncAudit tests, add more columns for richer audit

--- src/main/kotlin/dartzee/db/SyncAuditEntity.kt
+++ src/main/kotlin/dartzee/db/SyncAuditEntity.kt
@@ -1,5 +1,9 @@
 package dartzee.db
 
+import dartzee.`object`.DartsClient
+import dartzee.main.getDeviceId
+import dartzee.main.getUsername
+import dartzee.utils.DARTS_VERSION_NUMBER
 import dartzee.utils.Database
 import dartzee.utils.InjectedThings
 import java.sql.Timestamp
@@ -7,15 +11,39 @@ import java.sql.Timestamp
 class SyncAuditEntity(database: Database = InjectedThings.mainDatabase) : AbstractEntity<SyncAuditEntity>(database)
 {
     var remoteName = ""
+    var username = ""
+    var appVersion = ""
+    var deviceId = ""
+    var operatingSystem = ""
 
     override fun getTableName() = "SyncAudit"
 
-    override fun getCreateTableSqlSpecific() = "RemoteName VARCHAR(255) NOT NULL"
+    override fun getCreateTableSqlSpecific() =
+        "RemoteName VARCHAR(255) NOT NULL, Username VARCHAR(1000) NOT NULL, AppVersion VARCHAR(255) NOT NULL, " +
+        "DeviceId VARCHAR(36) NOT NULL, OperatingSystem VARCHAR(1000) NOT NULL"
 
-    fun getLastSyncDate(remoteName: String): Timestamp?
+    companion object
     {
-        val entities = retrieveEntities("RemoteName = '$remoteName'")
-        val latest = entities.maxBy { it.dtLastUpdate }
-        return latest?.dtLastUpdate
+        fun insertSyncAudit(database: Database, remoteName: String)
+        {
+            val entity = SyncAuditEntity(database)
+            entity.assignRowId()
+            entity.appVersion = DARTS_VERSION_NUMBER
+            entity.deviceId = getDeviceId()
+            entity.username = getUsername()
+            entity.operatingSystem = DartsClient.operatingSystem
+            entity.remoteName = remoteName
+
+            entity.saveToDatabase()
+        }
+
+        fun getLastSyncDate(database: Database, remoteName: String): Timestamp?
+        {
+            val dao = SyncAuditEntity(database)
+            val entities = dao.retrieveEntities("RemoteName = '$remoteName'")
+            val latest = entities.maxBy { it.dtLastUpdate }
+            return latest?.dtLastUpdate
+        }
     }
+
 }
