75813d79d8698aef9d499464a2018eb93992bc5d
alexburlton <alexburlton@hotmail.com>
2020-10-27 19:58:49 +0000

    GamesWon tests

--- src/test/kotlin/dartzee/achievements/TestAbstractAchievementGamesWon.kt
+++ src/test/kotlin/dartzee/achievements/TestAbstractAchievementGamesWon.kt
@@ -10,7 +10,7 @@ import io.kotlintest.shouldBe
 import org.junit.Test
 import java.sql.Timestamp
 
-abstract class TestAbstractAchievementGamesWon<E: AbstractAchievementGamesWon>: AbstractAchievementTest<E>()
+abstract class TestAbstractAchievementGamesWon<E: AbstractAchievementGamesWon>: TestAbstractAchievementRowPerGame<E>()
 {
     override fun setUpAchievementRowForPlayerAndGame(p: PlayerEntity, g: GameEntity)
     {
@@ -30,34 +30,30 @@ abstract class TestAbstractAchievementGamesWon<E: AbstractAchievementGamesWon>: 
     }
 
     @Test
-    fun `Should group by player, and take their latest finish date as DtLastUpdate`()
+    fun `Should insert a row per player and game, and take their latest finish date as DtLastUpdate`()
     {
         val alice = insertPlayer(name = "Alice")
         val bob = insertPlayer(name = "Bob")
 
-        val game = insertRelevantGame()
+        insertParticipant(gameId = insertRelevantGame().rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(500))
+        insertParticipant(gameId = insertRelevantGame().rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(1500))
+        insertParticipant(gameId = insertRelevantGame().rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(1000))
 
-        insertParticipant(gameId = game.rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(500))
-        insertParticipant(gameId = game.rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(1500))
-        insertParticipant(gameId = game.rowId, playerId = alice.rowId, finishingPosition = 1, dtFinished = Timestamp(1000))
-
-        insertParticipant(gameId = game.rowId, playerId = bob.rowId, finishingPosition = 1, dtFinished = Timestamp(2000))
-        insertParticipant(gameId = game.rowId, playerId = bob.rowId, finishingPosition = 1, dtFinished = Timestamp(1000))
+        insertParticipant(gameId = insertRelevantGame().rowId, playerId = bob.rowId, finishingPosition = 1, dtFinished = Timestamp(2000))
+        insertParticipant(gameId = insertRelevantGame().rowId, playerId = bob.rowId, finishingPosition = 1, dtFinished = Timestamp(1000))
 
         factoryAchievement().populateForConversion("")
 
-        getCountFromTable("Achievement") shouldBe 2
+        getCountFromTable("Achievement") shouldBe 5
         val achievementRows = AchievementEntity().retrieveEntities("")
-        val aliceRow = achievementRows.find{ it.playerId == alice.rowId }!!
-        aliceRow.achievementCounter shouldBe 3
-        aliceRow.dtLastUpdate shouldBe Timestamp(1500)
-        aliceRow.gameIdEarned shouldBe ""
-        aliceRow.achievementDetail shouldBe ""
+        val aliceRows = achievementRows.filter { it.playerId == alice.rowId }
+        aliceRows.size shouldBe 3
+        aliceRows.forEach {
+            it.achievementCounter shouldBe -1
+            it.achievementDetail shouldBe ""
+        }
 
-        val bobRow = achievementRows.find{ it.playerId == bob.rowId }!!
-        bobRow.achievementCounter shouldBe 2
-        bobRow.dtLastUpdate shouldBe Timestamp(2000)
-        bobRow.gameIdEarned shouldBe ""
-        bobRow.achievementDetail shouldBe ""
+        val bobRow = achievementRows.filter { it.playerId == bob.rowId }
+        bobRow.size shouldBe 2
     }
 }
