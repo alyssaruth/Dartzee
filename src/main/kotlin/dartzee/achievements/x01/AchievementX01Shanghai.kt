eafa0479e37d9e385aa00fee8fcf29c04b070ef9
Alex Burlton <alexburlton@hotmail.com>
2020-11-05 21:44:01 +0000

    speed up and simplify shanghai conversion by reusing X01Rounds

--- src/main/kotlin/dartzee/achievements/x01/AchievementX01Shanghai.kt
+++ src/main/kotlin/dartzee/achievements/x01/AchievementX01Shanghai.kt
@@ -31,23 +31,16 @@ class AchievementX01Shanghai : AbstractMultiRowAchievement()
 
     override fun populateForConversion(players: List<PlayerEntity>, database: Database)
     {
+        ensureX01RoundsTableExists(players, database)
+
         val tempTable = database.createTempTable("Shanghai", "RoundNumber INT, ParticipantId VARCHAR(36), PlayerId VARCHAR(36), GameId VARCHAR(36)")
 
         var sb = StringBuilder()
         sb.append(" INSERT INTO $tempTable")
-        sb.append(" SELECT DISTINCT drtFirst.RoundNumber, pt.RowId, pt.PlayerId, pt.GameId")
-        sb.append(" FROM Dart drtFirst, Dart drtLast, Participant pt, Game g")
-        sb.append(" WHERE drtFirst.ParticipantId = pt.RowId")
-        sb.append(" AND drtFirst.PlayerId = pt.PlayerId")
-        sb.append(" AND drtLast.ParticipantId = pt.RowId")
-        sb.append(" AND drtLast.PlayerId = pt.PlayerId")
-        sb.append(" AND pt.GameId = g.RowId")
-        sb.append(" AND g.GameType = '${GameType.X01}'")
-        sb.append(" AND drtFirst.Ordinal = 1")
-        sb.append(" AND drtLast.Ordinal = 3")
-        sb.append(" AND drtLast.RoundNumber = drtFirst.RoundNumber")
-        sb.append(" AND ${getTotalRoundScoreSql("drtFirst")} = 120")
-        appendPlayerSql(sb, players)
+        sb.append(" SELECT RoundNumber, ParticipantId, PlayerId, GameId")
+        sb.append(" FROM $X01_ROUNDS_TABLE")
+        sb.append(" WHERE TotalDartsThrown = 3")
+        sb.append(" AND StartingScore - RemainingScore = 120")
 
         if (!database.executeUpdate("" + sb)) return
 
