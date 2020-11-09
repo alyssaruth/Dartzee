9cefd07b65bb15a7a111e6d70d7e74d21146e730
alexburlton <alexburlton@hotmail.com>
2020-11-09 20:00:23 +0000

    ChangeLog for release

--- src/main/kotlin/dartzee/utils/ApplicationConstants.kt
+++ src/main/kotlin/dartzee/utils/ApplicationConstants.kt
@@ -1,5 +1,5 @@
 package dartzee.utils
 
-const val DARTS_VERSION_NUMBER = "v4.6.0"
+const val DARTS_VERSION_NUMBER = "v4.6.1"
 const val DARTZEE_REPOSITORY_URL = "https://api.github.com/repos/alexburlton/Dartzee"
 const val DARTZEE_MANUAL_DOWNLOAD_URL = "https://github.com/alexburlton/Dartzee/releases"
