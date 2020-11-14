2f1d387e8d5d3771f19cf19dfed748e9ddc789c4
Alex Burlton <alexburlton@hotmail.com>
2020-11-14 17:47:22 +0000

    fmt

--- terraform/aws_s3.tf
+++ terraform/aws_s3.tf
@@ -17,15 +17,15 @@ resource "aws_s3_bucket" "dartzee_databases" {
 module "unit_test_access" {
   source = "./modules/s3-read-write-access"
 
-  bucket_id = aws_s3_bucket.unit_test.id
+  bucket_id  = aws_s3_bucket.unit_test.id
   bucket_arn = aws_s3_bucket.unit_test.arn
-  user_arn = aws_iam_user.dartzee_sync_user.arn
+  user_arn   = aws_iam_user.dartzee_sync_user.arn
 }
 
 module "dartzee_databases_access" {
   source = "./modules/s3-read-write-access"
 
-  bucket_id = aws_s3_bucket.dartzee_databases.id
+  bucket_id  = aws_s3_bucket.dartzee_databases.id
   bucket_arn = aws_s3_bucket.dartzee_databases.arn
-  user_arn = aws_iam_user.dartzee_sync_user.arn
+  user_arn   = aws_iam_user.dartzee_sync_user.arn
 }
