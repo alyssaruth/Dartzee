55e122ffc1feb287bf5b1c31fda80a8b45240d88
Alex Burlton <alexburlton@hotmail.com>
2021-09-03 22:54:14 +0100

    spread tests

--- src/test/kotlin/dartzee/helper/DartboardSegmentFactory.kt
+++ src/test/kotlin/dartzee/helper/DartboardSegmentFactory.kt
@@ -3,6 +3,7 @@ package dartzee.helper
 import dartzee.`object`.DartboardSegment
 import dartzee.`object`.SegmentType
 
+fun miss(score: Int) = DartboardSegment(SegmentType.MISS, score)
 fun outerSingle(score: Int) = DartboardSegment(SegmentType.OUTER_SINGLE, score)
 fun double(score: Int) = DartboardSegment(SegmentType.DOUBLE, score)
 fun treble(score: Int) = DartboardSegment(SegmentType.TREBLE, score)
