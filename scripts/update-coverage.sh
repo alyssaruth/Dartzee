98bfccfc6d029fd6bc610e898572341682fccf53
Alex Burlton <alexburlton@hotmail.com>
2021-11-26 19:37:13 +0000

    rm bash script

--- scripts/update-coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-xmlReport="$scriptDir/../build/reports/kover/report.xml"
-
-lineCoverageXml=$(cat $xmlReport | grep "type=\"LINE\"" | tail -1)
-linesMissed=$(sed 's/.*missed=".*"//' <<< "$lineCoverageXml")
