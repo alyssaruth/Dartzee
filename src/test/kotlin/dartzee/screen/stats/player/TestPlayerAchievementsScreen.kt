e44b7ec46e7bb33ffb31dc52e9085df5c26cfe6c
Alex Burlton <alexburlton@hotmail.com>
2022-03-02 09:03:41 +0000

    more WIP

--- src/test/kotlin/dartzee/screen/stats/player/TestPlayerAchievementsScreen.kt
+++ src/test/kotlin/dartzee/screen/stats/player/TestPlayerAchievementsScreen.kt
@@ -1,5 +1,11 @@
 package dartzee.screen.stats.player
 
+import dartzee.achievements.AchievementType
+import dartzee.achievements.getAchievementMaximum
+import dartzee.achievements.x01.AchievementX01BestFinish
+import dartzee.achievements.x01.AchievementX01HighestBust
+import dartzee.db.AchievementEntity
+import dartzee.helper.insertGame
 import dartzee.helper.insertPlayer
 import dartzee.screen.ScreenCache
 import io.kotlintest.shouldBe
@@ -21,6 +27,19 @@ class TestPlayerAchievementsScreen
 
     @Test
     fun `Should update title with player name and achievement progress`()
+    {
+        val p = insertPlayer(name = "Bob")
+        val g = insertGame()
+
+        AchievementEntity.updateAchievement(AchievementType.X01_BEST_FINISH, p.rowId, g.rowId, AchievementX01BestFinish().blueThreshold)
+        AchievementEntity.updateAchievement(AchievementType.X01_HIGHEST_BUST, p.rowId, g.rowId, AchievementX01HighestBust().pinkThreshold)
+
+        val achievementsScrn = ScreenCache.switchToAchievementsScreen(p)
+        achievementsScrn.getScreenName() shouldBe "Achievements - Bob - 11/${getAchievementMaximum()}"
+    }
+
+    @Test
+    fun `Should do things on hover`()
     {
 
     }
