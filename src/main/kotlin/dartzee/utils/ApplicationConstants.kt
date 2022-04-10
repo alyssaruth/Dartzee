6888ca079fc9383cd04fb70d162bd1967d15b594
Alyssa <alyssa.burlton@gmail.com>
2022-04-10 09:55:13 +0100

    prep for 5.3.0 release

--- src/main/kotlin/dartzee/utils/ApplicationConstants.kt
+++ src/main/kotlin/dartzee/utils/ApplicationConstants.kt
@@ -1,6 +1,6 @@
 package dartzee.utils
 
-const val DARTS_VERSION_NUMBER = "v5.2.0"
+const val DARTS_VERSION_NUMBER = "v5.3.0"
 const val DARTZEE_REPOSITORY_URL_2 = "https://api.github.com/repos/alexburlton/Dartzee"
 const val DARTZEE_REPOSITORY_URL = "https://api.github.com/repos/alyssaburlton/Dartzee"
 const val DARTZEE_MANUAL_DOWNLOAD_URL = "https://github.com/alexburlton/Dartzee/releases"
