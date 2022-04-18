9eef9b71f2fa149c39f03e8b94389829567968d3
Alyssa <alyssa.burlton@gmail.com>
2022-04-18 20:20:14 +0100

    Remove redundant test

--- src/test/kotlin/dartzee/screen/TestAboutDialog.kt
+++ src/test/kotlin/dartzee/screen/TestAboutDialog.kt
@@ -1,7 +1,6 @@
 package dartzee.screen
 
 import com.github.alexburlton.swingtest.doClick
-import com.github.alexburlton.swingtest.doMouseMove
 import com.github.alexburlton.swingtest.getChild
 import dartzee.helper.AbstractTest
 import dartzee.utils.InjectedThings
@@ -9,7 +8,6 @@ import io.kotlintest.shouldBe
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
-import java.awt.Cursor
 import javax.swing.JLabel
 
 class TestAboutDialog: AbstractTest()
@@ -28,16 +26,5 @@ class TestAboutDialog: AbstractTest()
         dlg.isVisible shouldBe false
 
         verify { InjectedThings.showChangeLog() }
-    }
-
-    @Test
-    fun `Should treat the changelog label like a hyperlink`()
-    {
-        val dlg = AboutDialog()
-
-        val lbl = dlg.getChild<JLabel> { it.text.contains("Change Log") }
-        lbl.doMouseMove()
-
-        dlg.cursor.type shouldBe Cursor.HAND_CURSOR
     }
 }
