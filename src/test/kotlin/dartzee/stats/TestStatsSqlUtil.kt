f7daf6cd392b393b3a29bd4c4168e467aea90ae3
Alyssa <alyssa.burlton@gmail.com>
2022-11-24 07:52:06 +0000

    WIP

--- src/test/kotlin/dartzee/stats/TestStatsSqlUtil.kt
+++ src/test/kotlin/dartzee/stats/TestStatsSqlUtil.kt
@@ -2,8 +2,13 @@ package dartzee.stats
 
 import dartzee.game.GameType
 import dartzee.helper.AbstractTest
+import dartzee.helper.insertGame
 import dartzee.helper.insertGameForPlayer
+import dartzee.helper.insertIntoDatabase
+import dartzee.helper.insertParticipant
 import dartzee.helper.insertPlayer
+import dartzee.helper.randomGuid
+import dartzee.`object`.Dart
 import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -31,5 +36,29 @@ class TestStatsSqlUtil : AbstractTest()
         bCounts.getCount(GameType.X01) shouldBe 1
         bCounts.getCount(GameType.GOLF) shouldBe 0
         bCounts.getCount(GameType.ROUND_THE_CLOCK) shouldBe 1
+    }
+
+    @Test
+    fun `Should retrieve a single game wrapper successfully`()
+    {
+        val game = insertGame(gameType = GameType.GOLF, gameParams = "9")
+        val player = insertPlayer()
+        val pt = insertParticipant(playerId = player.rowId, gameId = game.rowId)
+
+        val rounds = listOf(
+            listOf(Dart(1, 0), Dart(1, 3)),
+            listOf(Dart(17, 1), Dart(15, 1), Dart(15, 2)),
+            listOf(Dart(3, 2))
+        )
+        rounds.insertIntoDatabase(player, pt)
+
+        retrieveGameData(player.rowId, GameType.X01).size shouldBe 0
+        retrieveGameData(randomGuid(), GameType.GOLF).size shouldBe 0
+
+        val map = retrieveGameData(player.rowId, GameType.GOLF)
+        val wrapper = map[game.localId]!!
+        wrapper.gameParams shouldBe "9"
+        wrapper.finalScore shouldBe -1
+        wrapper.dartEntities.size shouldBe 6
     }
 }
