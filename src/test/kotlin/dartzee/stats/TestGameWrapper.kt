a753e0a34cb0cb7ce54f351d89ec8187b440ad25
Alyssa <alyssa.burlton@gmail.com>
2022-11-27 17:32:18 +0000

    tests for the stats tab itself

--- src/test/kotlin/dartzee/stats/TestGameWrapper.kt
+++ src/test/kotlin/dartzee/stats/TestGameWrapper.kt
@@ -1,6 +1,8 @@
 package dartzee.stats
 
 import dartzee.helper.AbstractTest
+import dartzee.helper.GAME_WRAPPER_301_1
+import dartzee.helper.GAME_WRAPPER_301_2
 import dartzee.helper.makeDart
 import dartzee.helper.makeGameWrapper
 import dartzee.helper.makeX01Rounds
@@ -63,34 +65,10 @@ class TestGameWrapper : AbstractTest()
     @Test
     fun `Should populate the three dart score map correctly`()
     {
-        val gameOneRounds = makeX01Rounds(
-            301,
-            listOf(Dart(20, 3), Dart(20, 2), Dart(20, 1)), // 120 (181)
-            listOf(Dart(20, 1), Dart(5, 1), Dart(20, 1)), // 45   (136)
-            listOf(Dart(20, 1), Dart(20, 1), Dart(20, 1)), // 60   (76)
-            listOf(Dart(12, 3), Dart(20, 2)) // 76 (0)
-        )
-
-        val gameTwoRounds = makeX01Rounds(
-            301,
-            listOf(Dart(5, 1), Dart(20, 1), Dart(20, 1)), // 45 (256)
-            listOf(Dart(12, 3), Dart(20, 1), Dart(4, 1)), // 60 (196)
-            listOf(Dart(19, 1), Dart(19, 1), Dart(19, 1)), // 57 (139)
-            listOf(Dart(1, 0), Dart(1, 1), Dart(1, 0)), // 1     (138)
-            listOf(Dart(20, 1), Dart(20, 1), Dart(18, 1)), // 58  (80)
-            listOf(Dart(20, 2), Dart(20, 2)) // 80 (0)
-        )
-
-        val wrapperOne = makeGameWrapper(gameParams = "301", finalScore = 11, localId = 1L)
-        gameOneRounds.flatten().forEach(wrapperOne::addDart)
-
-        val wrapperTwo = makeGameWrapper(gameParams = "301", finalScore = 17, localId = 2L)
-        gameTwoRounds.flatten().forEach(wrapperTwo::addDart)
-
         // Full map with the lowest threshold possible
         val map = mutableMapOf<Int, ThreeDartScoreWrapper>()
-        wrapperOne.populateThreeDartScoreMap(map, 62)
-        wrapperTwo.populateThreeDartScoreMap(map, 62)
+        GAME_WRAPPER_301_1.populateThreeDartScoreMap(map, 62)
+        GAME_WRAPPER_301_2.populateThreeDartScoreMap(map, 62)
 
         map.keys.shouldContainExactlyInAnyOrder(120, 60, 58, 57, 45, 1)
 
@@ -105,8 +83,8 @@ class TestGameWrapper : AbstractTest()
 
         // Higher threshold - test rounds are knocked out
         val shorterMap = mutableMapOf<Int, ThreeDartScoreWrapper>()
-        wrapperOne.populateThreeDartScoreMap(shorterMap, 140)
-        wrapperTwo.populateThreeDartScoreMap(shorterMap, 140)
+        GAME_WRAPPER_301_1.populateThreeDartScoreMap(shorterMap, 140)
+        GAME_WRAPPER_301_2.populateThreeDartScoreMap(shorterMap, 140)
 
         shorterMap.keys.shouldContainExactlyInAnyOrder(120, 60, 57, 45)
     }
