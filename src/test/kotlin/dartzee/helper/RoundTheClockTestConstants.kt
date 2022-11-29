4168c3c304e262a3652d8365611809836f6aaf67
Alyssa <alyssa.burlton@gmail.com>
2022-11-29 13:07:14 +0000

    finish tests for RTC tab

--- src/test/kotlin/dartzee/helper/RoundTheClockTestConstants.kt
+++ src/test/kotlin/dartzee/helper/RoundTheClockTestConstants.kt
@@ -19,8 +19,28 @@ val inOrderGameDarts = makeClockRounds(
 
 val GAME_WRAPPER_RTC_IN_ORDER = makeGameWrapper(
     gameParams = RoundTheClockConfig(ClockType.Standard, true).toJson(),
-    finalScore = 29
+    finalScore = inOrderGameDarts.size
 ).also { inOrderGameDarts.forEach(it::addDart) }
+
+val inOrderGameDarts2 = makeClockRounds(
+    true,
+    Dart(20, 1), Dart(18, 1), Dart(5, 1),
+    Dart(1, 0), Dart(1, 3), Dart(2, 1),
+    Dart(3, 0), Dart(19, 3), Dart(17, 1),
+    Dart(3, 1), Dart(4, 1), Dart(5, 1), Dart(6, 1),
+    Dart(7, 0), Dart(7, 1), Dart(8, 0),
+    Dart(8, 1), Dart(9, 1), Dart(10, 1),
+    Dart(14, 1), Dart(11, 1), Dart(12, 2),
+    Dart(13, 3), Dart(11, 1), Dart(14, 1),
+    Dart(15, 3), Dart(16, 1), Dart(17, 1), Dart(18, 0),
+    Dart(18, 0), Dart(18, 1), Dart(19, 1),
+    Dart(20, 0), Dart(20, 2)
+)
+
+val GAME_WRAPPER_RTC_IN_ORDER_2 = makeGameWrapper(
+    gameParams = RoundTheClockConfig(ClockType.Standard, true).toJson(),
+    finalScore = inOrderGameDarts2.size
+).also { inOrderGameDarts2.forEach(it::addDart) }
 
 val outOfOrderGameDarts = makeClockRounds(
     false,
--- src/test/kotlin/dartzee/screen/stats/player/rtc/TestStatisticsTabRoundTheClockHitRate.kt
+++ src/test/kotlin/dartzee/screen/stats/player/rtc/TestStatisticsTabRoundTheClockHitRate.kt
@@ -2,8 +2,10 @@ package dartzee.screen.stats.player.rtc
 
 import com.github.alexburlton.swingtest.getChild
 import dartzee.core.bean.ScrollTable
+import dartzee.getFooterRow
 import dartzee.getRows
 import dartzee.helper.GAME_WRAPPER_RTC_IN_ORDER
+import dartzee.helper.GAME_WRAPPER_RTC_IN_ORDER_2
 import dartzee.helper.GAME_WRAPPER_RTC_OUT_OF_ORDER
 import dartzee.helper.makeClockGameWrapper
 import dartzee.screen.stats.player.AbstractStatsPieBreakdownTest
@@ -26,5 +28,21 @@ class TestStatisticsTabRoundTheClockHitRate: AbstractStatsPieBreakdownTest<Stati
         val breakdownTable = tab.getChild<ScrollTable> { it.testId == "BreakdownMine" }
         val rows = breakdownTable.getRows()
         rows[0].shouldContainExactly(1, 1, 0, 0, 0, 0, 0, 0, 1.0)
+    }
+
+    @Test
+    fun `Should populate with correct data`()
+    {
+        val tab = factoryTab()
+        tab.setFilteredGames(listOf(GAME_WRAPPER_RTC_IN_ORDER, GAME_WRAPPER_RTC_IN_ORDER_2), emptyList())
+        tab.populateStats()
+
+        val breakdownTable = tab.getChild<ScrollTable> { it.testId == "BreakdownMine" }
+        val rows = breakdownTable.getRows()
+        rows[0].shouldContainExactly(1, 1, 0, 0, 1, 0, 0, 0, 3.0)
+        rows[3].shouldContainExactly(4, 1, 1, 0, 0, 0, 0, 0, 1.5)
+
+        val overallRow = breakdownTable.getFooterRow()
+        overallRow.shouldContainExactly("Overall", 25, 10, 3, 2, 0, 0, 0, 1.575)
     }
 }
