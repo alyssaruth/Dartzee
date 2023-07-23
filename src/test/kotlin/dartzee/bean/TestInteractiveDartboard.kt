71d28a8e11cb681af44bee5d63f2d73d0586bad7
Alyssa <alyssa.burlton@gmail.com>
2023-07-23 19:50:39 +0100

    add tests

--- src/test/kotlin/dartzee/bean/TestInteractiveDartboard.kt
+++ src/test/kotlin/dartzee/bean/TestInteractiveDartboard.kt
@@ -7,8 +7,10 @@ import dartzee.`object`.DartboardSegment
 import dartzee.`object`.SegmentType
 import dartzee.screen.game.SegmentStatuses
 import dartzee.utils.getAllNonMissSegments
+import dartzee.utils.getAllSegmentsForDartzee
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import java.awt.Point
 
 class TestInteractiveDartboard : AbstractTest()
 {
@@ -59,11 +61,37 @@ class TestInteractiveDartboard : AbstractTest()
         dartboard.hoverSegment(DartboardSegment(SegmentType.OUTER_SINGLE, 1))
         dartboard.shouldMatchImage("hover-invalid-segment")
 
+        // Outer dartboard should also do nothing
+        dartboard.highlightDartboard(Point(200, 40))
+        dartboard.shouldMatchImage("hover-invalid-segment")
+
         dartboard.hoverSegment(DartboardSegment(SegmentType.INNER_SINGLE, 11))
         dartboard.shouldMatchImage("hover-valid-segment")
 
+        dartboard.hoverSegment(DartboardSegment(SegmentType.TREBLE, 5))
+        dartboard.shouldMatchImage("hover-valid-treble")
+
         dartboard.hoverSegment(DartboardSegment(SegmentType.OUTER_SINGLE, 20))
         dartboard.shouldMatchImage("hover-scoring-segment")
+    }
+
+    @Test
+    @Tag("screenshot")
+    fun `Hovering should toggle the outer dartboard if it is a valid segment`()
+    {
+        val scoringSegments = getAllNonMissSegments().filter { it.score % 2 == 1 }
+        val validSegments = getAllSegmentsForDartzee()
+        val segmentStatuses = SegmentStatuses(scoringSegments, validSegments)
+
+        val dartboard = InteractiveDartboard(DEFAULT_COLOUR_WRAPPER)
+        dartboard.setBounds(0, 0, 400, 400)
+        dartboard.updateSegmentStatus(segmentStatuses)
+
+        dartboard.highlightDartboard(Point(200, 40))
+        dartboard.shouldMatchImage("hover-outer-dartboard")
+
+        dartboard.highlightDartboard(Point(200, 0))
+        dartboard.shouldMatchImage("hover-nothing")
     }
 
     private fun InteractiveDartboard.hoverSegment(segment: DartboardSegment)
