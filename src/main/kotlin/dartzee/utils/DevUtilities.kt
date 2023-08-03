3b09cd562f2114a932c7a57b1c0b97bc69df08e0
Alyssa <alyssa.burlton@gmail.com>
2023-08-03 13:04:39 +0100

    plug in deletion of teams

--- src/main/kotlin/dartzee/utils/DevUtilities.kt
+++ src/main/kotlin/dartzee/utils/DevUtilities.kt
@@ -3,8 +3,11 @@ package dartzee.utils
 import dartzee.core.util.DialogUtil
 import dartzee.db.DartEntity
 import dartzee.db.GameEntity
+import dartzee.db.IParticipant
 import dartzee.db.ParticipantEntity
+import dartzee.db.TeamEntity
 import dartzee.db.X01FinishEntity
+import dartzee.game.loadParticipants
 import dartzee.screen.ScreenCache
 import dartzee.utils.InjectedThings.mainDatabase
 import javax.swing.JOptionPane
@@ -60,13 +63,20 @@ object DevUtilities
             return
         }
 
-        val participantIds = ParticipantEntity().retrieveEntities("GameId = '$gameId'").map { it.rowId }
+        val participants = loadParticipants(gameId)
+        val participantEntities: List<IParticipant> = participants.flatMap { it.individuals + it.participant }
+        val participantIds = participantEntities.filterIsInstance<ParticipantEntity>().map { it.rowId }
+        val teamIds = participantEntities.filterIsInstance<TeamEntity>().map { it.rowId }
         val ptSql = participantIds.getQuotedIdStr()
         val dartCount = if (participantIds.isEmpty()) 0 else DartEntity().countWhere("ParticipantId IN $ptSql")
 
-        val question = ("Purge all data for Game #$localId? The following rows will be deleted:"
-                + "\n\n Participant: ${participantIds.size} rows"
-                + "\n Dart: $dartCount rows")
+        val question = """
+            Purge all data for Game #$localId? The following rows will be deleted:
+            
+            Participant: ${participantIds.size} rows
+            Team: ${teamIds.size} rows
+            Dart: $dartCount rows
+        """.trimIndent()
 
         val answer = DialogUtil.showQuestionOLD(question, false)
         if (answer == JOptionPane.YES_OPTION)
@@ -76,6 +86,7 @@ object DevUtilities
                 DartEntity().deleteWhere("ParticipantId IN $ptSql")
             }
 
+            TeamEntity().deleteWhere("GameId = '$gameId'")
             ParticipantEntity().deleteWhere("GameId = '$gameId'")
             X01FinishEntity().deleteWhere("GameId = '$gameId'")
             GameEntity().deleteWhere("RowId = '$gameId'")
