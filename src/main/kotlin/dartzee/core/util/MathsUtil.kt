3c427cdb1f7870878e5482a0ed8324824ef4045a
Alyssa <alyssa.burlton@gmail.com>
2023-08-31 19:50:46 +0100

    UtilityClassWithPublicConstructor

--- src/main/kotlin/dartzee/core/util/MathsUtil.kt
+++ src/main/kotlin/dartzee/core/util/MathsUtil.kt
@@ -1,6 +1,7 @@
 package dartzee.core.util
 
 import kotlin.math.ceil
+import kotlin.math.pow
 
 
 fun Int.ceilDiv(other: Int) = ceil(this.toDouble() / other).toInt()
@@ -17,28 +18,25 @@ fun <T> Pair<Double, Double>.mapStepped(stepSize: Double, mapFunction: (Double) 
     return ret
 }
 
-class MathsUtil
+object MathsUtil
 {
-    companion object
+    fun round(number: Double, decimalPlaces: Int): Double
     {
-        fun round(number: Double, decimalPlaces: Int): Double
+        val powerOfTen = 10.0.pow(decimalPlaces.toDouble())
+
+        val rounded = Math.round(powerOfTen * number)
+
+        return rounded / powerOfTen
+    }
+
+    fun getPercentage(count: Number, total: Number, digits: Int = 1) =
+        getPercentage(count.toDouble(), total.toDouble(), digits)
+
+    fun getPercentage(count: Double, total: Double, digits: Int = 1): Double
+    {
+        return if (count == 0.0)
         {
-            val powerOfTen = Math.pow(10.0, decimalPlaces.toDouble())
-
-            val rounded = Math.round(powerOfTen * number)
-
-            return rounded / powerOfTen
-        }
-
-        fun getPercentage(count: Number, total: Number, digits: Int = 1) =
-            getPercentage(count.toDouble(), total.toDouble(), digits)
-
-        fun getPercentage(count: Double, total: Double, digits: Int = 1): Double
-        {
-            return if (count == 0.0)
-            {
-                0.0
-            } else round(100 * count / total, digits)
-        }
+            0.0
+        } else round(100 * count / total, digits)
     }
 }
