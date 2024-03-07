59102412845816ff681360fdbaa120e81267d205
Alyssa <alyssa.burlton@gmail.com>
2024-03-07 11:18:32 +0000

    change log etc for release

--- src/main/kotlin/dartzee/utils/ApplicationConstants.kt
+++ src/main/kotlin/dartzee/utils/ApplicationConstants.kt
@@ -1,5 +1,5 @@
 package dartzee.utils
 
-const val DARTS_VERSION_NUMBER = "v7.0.1"
+const val DARTS_VERSION_NUMBER = "v7.1.0"
 const val DARTZEE_REPOSITORY_URL = "https://api.github.com/repos/alyssaruth/Dartzee"
 const val DARTZEE_MANUAL_DOWNLOAD_URL = "https://github.com/alyssaruth/Dartzee/releases"
