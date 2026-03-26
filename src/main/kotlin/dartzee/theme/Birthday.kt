9a652bbebfde78e01b542974ab33b6f132f5bd97
alyssa <alyssa.burlton@gmail.com>
2026-03-26 18:31:51 +0000

    chocolate cake dartboard

--- src/main/kotlin/dartzee/theme/Birthday.kt
+++ src/main/kotlin/dartzee/theme/Birthday.kt
@@ -6,11 +6,22 @@ import java.awt.Color
 import java.awt.Point
 import javax.swing.ImageIcon
 
-private val aqua = Color.decode("#00FFF0")
-private val pink = Color.decode("#FF13F0")
+private val lightBrown = Color.decode("#542e15")
+private val darkBrown = Color.decode("#4b270f")
+private val lightPink = Color.decode("#b25f67")
+private val darkPink = Color.decode("#813c3f")
 
 private val dartboardColours =
-    ColourWrapper(Color.white, pink, pink, Color.white, aqua, aqua, aqua, pink)
+    ColourWrapper(
+        lightBrown,
+        lightPink,
+        darkPink,
+        darkBrown,
+        darkPink,
+        lightPink,
+        darkPink,
+        lightPink,
+    )
 
 private fun dartFactory(pt: Point): DartLabel {
     val icon = ImageIcon(Theme::class.java.getResource("/theme/birthday/darts/candle-red.png"))
