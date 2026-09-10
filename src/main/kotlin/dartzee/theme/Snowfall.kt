975258822cf8cb0bafa5d74f5cf8ff31ab176a80
alyssa <alyssa.burlton@gmail.com>
2026-09-10 18:56:00 +0100

    terribly performing snow

--- src/main/kotlin/dartzee/theme/Snowfall.kt
+++ src/main/kotlin/dartzee/theme/Snowfall.kt
@@ -8,15 +8,22 @@ import javax.swing.SwingUtilities
 import kotlin.random.Random
 
 class Snowfall : JComponent() {
+    private val snowHeap: MutableMap<Int, List<Color>> = mutableMapOf()
 
-    data class Particle(val speed: Int, val x: Int, val y: Int, val color: Color)
+    data class Particle(
+        val speed: Int,
+        val windEffect: Int,
+        val x: Int,
+        val y: Int,
+        val color: Color,
+    )
 
     private var particles = listOf<Particle>()
 
     init {
         val r = Runnable {
             while (true) {
-                Thread.sleep(200)
+                Thread.sleep(50)
                 SwingUtilities.invokeAndWait { doTick() }
             }
         }
@@ -37,37 +44,91 @@ class Snowfall : JComponent() {
             g.paint = snow.color
             g.drawLine(snow.x, snow.y, snow.x, snow.y)
         }
+
+        snowHeap.forEach { (x, snowflakes) ->
+            snowflakes.forEachIndexed { index, color ->
+                g.paint = color
+                g.drawLine(x, height - index, x, height - index)
+            }
+        }
     }
 
     private fun doTick() {
         val updatedParticles = particles.map { snow ->
-            val windEffect = Random.nextInt(-10, 10)
+            val windEffect = Random.nextInt(-10, 11)
+            val windImpact = windEffect / 10
+
+            val newWindDirection =
+                if (windImpact == 0) 0 else if (snow.windEffect == -windImpact) 0 else windImpact
             val newY = snow.y + snow.speed
-            val newX = snow.x + windEffect / 10
-            snow.copy(x = newX, y = newY)
+            val newX = snow.x + newWindDirection
+            snow.copy(x = newX, y = newY, windEffect = newWindDirection)
         }
 
         val newParticles =
             (0..width).mapNotNull { x ->
-                val spawnSnow = Random.nextInt(20) >= 19
+                val spawnSnow = Random.nextInt(100) >= 99
                 if (spawnSnow) {
                     val baseColor = Color.WHITE
-                    val offness = Random.nextInt(10)
+                    val offness = Random.nextInt(100)
                     val color =
                         Color(
                             baseColor.red - offness,
                             baseColor.green - offness,
                             baseColor.blue - offness,
                         )
-                    val speed = if (Random.nextInt(10) >= 9) 2 else 1
-                    Particle(speed, x, 0, color)
+                    val speed = if (Random.nextInt(10) >= 11) 2 else 1
+                    Particle(speed, 0, x, 0, color)
                 } else {
                     null
                 }
             }
 
-        particles = updatedParticles + newParticles
+        val newHeapParticles = updatedParticles.filter {
+            (height - snowHeap.getOrDefault(it.x, emptyList()).size) == it.y || it.y >= height
+        }
+        val fallingParticles = updatedParticles - newHeapParticles
+
+        particles = fallingParticles + newParticles
+
+        newHeapParticles.forEach {
+            val current = snowHeap.getOrDefault(it.x, emptyList())
+            snowHeap[it.x] = current + it.color
+        }
+
+        smoothHeap()
 
         repaint()
     }
+
+    private fun smoothHeap() {
+        val xValues = snowHeap.keys
+        val peaks = xValues.filter { x ->
+            val height = snowHeap.getSnowCount(x)
+
+            val left = snowHeap.getSnowCount(x - 1)
+            val right = snowHeap.getSnowCount(x + 1)
+
+            height > left + 1 || height > right + 1
+        }
+
+        peaks.forEach { x ->
+            val current = snowHeap.getValue(x)
+            val flake = current.last()
+            snowHeap[x] = current.dropLast(1)
+
+            val left = snowHeap.getOrDefault(x - 1, emptyList())
+            val right = snowHeap.getOrDefault(x + 1, emptyList())
+
+            val direction = Random.nextInt(2)
+            if (direction == 0 && right.size < current.size - 1) {
+                snowHeap[x + 1] = right + flake
+            } else {
+                snowHeap[x - 1] = left + flake
+            }
+        }
+    }
+
+    private fun MutableMap<Int, List<Color>>.getSnowCount(x: Int) =
+        getOrDefault(x, emptyList()).size
 }
